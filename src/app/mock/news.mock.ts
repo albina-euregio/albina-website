da57601e1c9e32bc2837f46e217e6a17a6dd5e64
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2017-04-23 15:26:09 +0200

    Add mocks for news and bulletins.

---
+++ src/app/mock/news.mock.ts
@@ -0,0 +1,33 @@
+export const MockNews = [
+  {
+    'date': '2017-04-23T15:00:23+01:00',
+    'title': [
+      {
+        'languageCode': 'it',
+        'text': 'Bollettino ALBINA'
+      },
+      {
+        'languageCode': 'de',
+        'text': 'ALBINA Lagebericht'
+      },
+      {
+        'languageCode': 'en',
+        'text': 'ALBINA Bulletin'
+      }
+    ],
+    'content': [
+      {
+        'languageCode': 'it',
+        'text': 'Mi chiamo ALBINA ...'
+      },
+      {
+        'languageCode': 'de',
+        'text': 'Mein Name ist ALBINA ...'
+      },
+      {
+        'languageCode': 'en',
+        'text': 'My name is ALBINA ...'
+      }
+    ]
+  }
+]
