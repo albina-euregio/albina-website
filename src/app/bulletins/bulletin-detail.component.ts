13fea4535cb4c9db0adb80273051742dcd045206
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2017-07-31 11:16:10 +0200

    Allow selection of aspect range.

--- src/app/bulletins/bulletin-detail.component.ts
+++ src/app/bulletins/bulletin-detail.component.ts
@@ -50,14 +50,43 @@ export class BulletinDetailComponent {
   }
 
   selectAspect(aspect) {
-    if (this.bulletinElevationDescription.containsAspect(Enums.Aspect[aspect])) {
-      this.bulletinElevationDescription.removeAspect(Enums.Aspect[aspect]);
-      document.getElementById(Enums.Aspect[aspect]).style.fill = "#FFFFFF";
-      document.getElementById(Enums.Aspect[aspect]).focus();
+    if (this.bulletinElevationDescription.aspects.length == 1) {
+      let a : any = Enums.Aspect[this.bulletinElevationDescription.aspects[0]];
+
+      if (a == aspect || a == aspect + 1) {
+        for (var i = 0; i < 8; i++) {
+          this.bulletinElevationDescription.addAspect(Enums.Aspect[i]);
+          document.getElementById(Enums.Aspect[i]).style.fill = "#000000";
+          document.getElementById(Enums.Aspect[i]).focus();
+        }
+      } else {
+        for (var i = 0; i < 8; i++) {
+          document.getElementById(Enums.Aspect[i]).style.fill = "#FFFFFF";
+          document.getElementById(Enums.Aspect[i]).focus();
+        }
+
+        let end = (aspect + 1) % 8;
+        while (a != end) {
+          this.bulletinElevationDescription.addAspect(Enums.Aspect[a]);
+          document.getElementById(Enums.Aspect[a]).style.fill = "#000000";
+          document.getElementById(Enums.Aspect[a]).focus();
+          a = (a + 1) % 8;
+        }
+      }
     } else {
-  		this.bulletinElevationDescription.addAspect(Enums.Aspect[aspect]);
+      this.bulletinElevationDescription.setAspects(new Array<Enums.Aspect>());
+      for (var i = 0; i < 8; i++) {
+        document.getElementById(Enums.Aspect[i]).style.fill = "#FFFFFF";
+        document.getElementById(Enums.Aspect[i]).focus();
+      }
+
+      this.bulletinElevationDescription.addAspect(Enums.Aspect[aspect]);
       document.getElementById(Enums.Aspect[aspect]).style.fill = "#000000";
       document.getElementById(Enums.Aspect[aspect]).focus();
     }
+
+    console.log("Aspects:");
+    for (let a of this.bulletinElevationDescription.aspects)
+      console.log(a);
   }
 }
