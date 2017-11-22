7016110ff6d2da5bc56721cf8b539d1dea25ac41
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2017-11-22 14:47:59 +0100

    Add server url of clesius.

--- src/app/providers/constants-service/constants.service.ts
+++ src/app/providers/constants-service/constants.service.ts
@@ -3,12 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConstantsService {
 
+  // Localhost
   //public serverUrl: string = 'http://localhost:8080/albina/api/';
-  public serverUrl: string = 'http://212.47.231.185:8080/albina/api/';
-  
   //public socketIOUrl: string = 'http://localhost:9092';
-  //public socketIOUrl: string = 'http://127.0.0.1:9092';
+
+  // Development server
+  public serverUrl: string = 'http://212.47.231.185:8080/albina/api/';
   public socketIOUrl: string = 'http://212.47.231.185:9092';
+
+  // CLESIUS
+  //public serverUrl: string = 'http://192.168.30.189:8080/albina/api/';
+  //public socketIOUrl: string = 'http://192.168.30.189:9092';
 
   public codeTyrol: string = 'AT-07';
   public codeSouthTyrol: string = 'IT-32-BZ';
