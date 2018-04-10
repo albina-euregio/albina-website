bb9ed95cf52fe1a3754446f8c31e3eabacc5d22f
margonimarco <margoni@clesius.it>
2018-04-10 16:13:13 +0200

    textcat Integration

--- src/app/bulletins/cop.component.ts
+++ src/app/bulletins/cop.component.ts
@@ -1,53 +1,59 @@
 import { Component, Input, ElementRef, ViewChild } from '@angular/core';
 
+
 @Component({
-	selector: 'cop',
+  selector: 'cop',
   templateUrl: 'cop.component.html'
 })
+
 export class CopComponent {
 
-	@Input() text: string;
+  @Input() text: string;
   @Input() disabled: boolean;
 
   @ViewChild('textcat') textcat: ElementRef;
 
   loadAPI: Promise<any>;
-
-  constructor() {
+  
+  constructor() {    
   }
 
   ngOnInit() {
+
   }
 
   ngOnChanges() {
     this.loadAPI = new Promise((resolve) => {
-       // this.loadScript();
-        resolve(true);
+      //this.loadScript();
+      resolve(true);
     });
   }
 
-  public loadScript() {
-    var isFound = false;
-    var scripts = document.getElementsByTagName("script")
-    for (var i = 0; i < scripts.length; ++i) {
-      if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
-          isFound = true;
-      }
-    }
 
-    if (!isFound) {
-      var dynamicScripts = [
-        "assets/javascript/phrasemaker_it.js"
-      ];
 
-      for (var i = 0; i < dynamicScripts .length; i++) {
-        let node = document.createElement('script');
-        node.src = dynamicScripts [i];
-        node.type = 'text/javascript';
-        node.async = false;
-        node.charset = 'utf-8';
-        document.getElementsByTagName('head')[0].appendChild(node);
+  /*
+    public loadScript() {
+      var isFound = false;
+      var scripts = document.getElementsByTagName("script")
+      for (var i = 0; i < scripts.length; ++i) {
+        if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
+            isFound = true;
+        }
       }
-    }
-  }
+  
+      if (!isFound) {
+        var dynamicScripts = [
+       //   "assets/javascript/phrasemaker_it.js"
+        ];
+  
+        for (var i = 0; i < dynamicScripts .length; i++) {
+          let node = document.createElement('script');
+          node.src = dynamicScripts [i];
+          node.type = 'text/javascript';
+          node.async = false;
+          node.charset = 'utf-8';
+          document.getElementsByTagName('head')[0].appendChild(node);
+        }
+      }*/
 }
+
