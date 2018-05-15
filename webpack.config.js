a828c036fc3231c7b105eded1dd3a5507dcf4a95
adammertel <mertel.adam@gmail.com>
2018-05-15 12:52:44 +0200

    webpack works

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,69 @@
-const webpack = require('webpack');
-const path = require('path');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
-    contentBase: './app',
-    port: 8080
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    path.resolve(__dirname, 'app/main.jsx')
-  ],
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
-    filename: './bundle.js'
+  entry: {
+    app: './app/main.jsx'
   },
   module: {
-    loaders: [
+    rules: [
+      {
+        test: /\.js$|\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
       {
         test: /\.css$/,
-        include: path.resolve(__dirname, 'app'),
-        loader: 'style-loader!css-loader'
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
-        test: /\.js[x]?$/,
-        include: path.resolve(__dirname, 'app'),
-        exclude: /node_modules/,
-        loader: 'babel-loader'
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'sass-loader'
+        ]
       },
       {
-        test: /\.(ttf|eot|woff|woff2)$/,
-        loader: 'file-loader',
-        options: {
-          name: 'fonts/[name].[ext]'
-        }
-      },
-      {
-        test: /\.(png|jpeg|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: 'images/[name].[ext]'
-        }
+        test: /\.(jpe?g|png|gif|svg|eot|ttf|svg|woff|woff2|ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './assets/',
+              publicPath: './assets/'
+            }
+          }
+        ]
       }
     ]
   },
-  resolve: {
-    extensions: ['.jsx', '.js', '.json'],
-    modules: ['node_modules']
-  },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_CONFIG_DIR': './config'
-    })
-  ],
-  node: {
-    fs: 'empty'
-  }
+    new HtmlWebPackPlugin({
+      template: './app/index.html',
+      filename: './index.html',
+      favicon: './app/images/fav/en/favicon.ico'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new CopyWebpackPlugin([{ from: './app/data', to: 'data' }], {})
+  ]
 };
