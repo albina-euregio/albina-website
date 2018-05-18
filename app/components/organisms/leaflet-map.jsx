7319e12df8470bea32be671e63dc11460a4f3027
adammertel <mertel.adam@gmail.com>
2018-05-18 09:52:38 +0200

    in-house tilemap

--- app/components/organisms/leaflet-map.jsx
+++ app/components/organisms/leaflet-map.jsx
@@ -46,8 +46,10 @@ class BulletinMap extends React.Component {
       >
         <LayerGroup>
           <TileLayer
-            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-            maxZoom={19}
+            url="https://lawis.at/wms/LAWIS/LAWIS-TMS_bw/{z}/{x}/{y}.png"
+            maxZoom={10}
+            detectRetina={false}
+            tms={true}
             attribution="&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>, Tiles courtesy of <a href='http://hot.openstreetmap.org/' target='_blank'>Humanitarian OpenStreetMap Team</a>"
             OpenStreetMap
           />
