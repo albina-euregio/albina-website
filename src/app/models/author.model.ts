4a4e0c1c53d276127a4218f033982e10adc76cd3
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2018-07-23 12:07:04 +0200

    Do not add regions and roles to json export of author.

--- src/app/models/author.model.ts
+++ src/app/models/author.model.ts
@@ -111,20 +111,6 @@ export class AuthorModel {
 			json['phone'] = this.phone;
 		if (this.organization && this.organization != undefined && this.organization != "")
 			json['organization'] = this.organization;
-		if (this.roles && this.roles.length > 0) {
-			let roles = [];
-			for (let i = 0; i <= this.roles.length - 1; i++) {
-				roles.push(this.roles[i]);
-			}
-			json['roles'] = roles;
-		}
-		if (this.regions && this.regions.length > 0) {
-			let regions = [];
-			for (let i = 0; i <= this.regions.length - 1; i++) {
-				regions.push(this.regions[i]);
-			}
-			json['regions'] = regions;
-		}
 
 		return json;
 	}
