21fe259619510da542b2349cb654c530cdb78cfe
adammertel <mertel.adam@gmail.com>
2018-08-27 21:30:17 +0200

    styling unsupported browser message

---
+++ app/components/dialogs/unsupported-browser-dialog.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { inject } from 'mobx-react'
+import { injectIntl, FormattedHTMLMessage } from 'react-intl'
+
+class UnsupportedBrowserDialog extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <div>
+
+        <div className='mfp-bg mfp-slide-animation mfp-ready' />
+        <div
+          className='modal mfp-content'
+          style={{
+            position: 'fixed',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '50%'
+          }}
+        >
+          <div className='modal-container'>
+            <div className='modal-header'>
+              <h2 className='subheader'>
+                <FormattedHTMLMessage id='dialog:unsupportedBrowser:heading' />
+              </h2>
+              <h2><FormattedHTMLMessage id='dialog:unsupportedBrowser:message' /></h2>
+            </div>
+            <a onClick={appStore.unsupportedBrowserModalOff.bind(appStore)}>
+              <button className='inverse pure-button'>
+                {this.props.intl.formatMessage({
+                  id: 'dialog:unsupportedBrowser:accept'
+                })}
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default inject('locale')(injectIntl(UnsupportedBrowserDialog))
--- app/components/page.jsx
+++ app/components/page.jsx
@@ -1,93 +1,96 @@
-import React from 'react';
-import { withRouter } from 'react-router';
-import PageLoadingScreen from './organisms/page-loading-screen.jsx';
-import Jumpnav from './organisms/jumpnav.jsx';
-import PageHeader from './organisms/page-header.jsx';
-import PageFooter from './organisms/page-footer.jsx';
-import MenuStore from '../stores/menuStore';
-import ModalDialog from './modal-dialog';
-import SubscribeDialog from './dialogs/subscribe-dialog';
-import SubscribeSocialMediaDialog from './dialogs/subscribe-social-media-dialog';
-import SubscribeAppDialog from './dialogs/subscribe-app-dialog';
-import SubscribeBulletinDialog from './dialogs/subscribe-bulletin-dialog';
-import SubscribeBlogDialog from './dialogs/subscribe-blog-dialog';
-import CookieConsent from './dialogs/cookie-consent';
-import { renderRoutes } from 'react-router-config';
-import { modal_init } from '../js/modal';
-import { tooltip_init } from '../js/tooltip';
-import { navigation_init } from '../js/navigation';
-import { video_init } from '../js/video';
+import React from 'react'
+import { withRouter } from 'react-router'
+import PageLoadingScreen from './organisms/page-loading-screen.jsx'
+import Jumpnav from './organisms/jumpnav.jsx'
+import PageHeader from './organisms/page-header.jsx'
+import PageFooter from './organisms/page-footer.jsx'
+import MenuStore from '../stores/menuStore'
+import { observer } from 'mobx-react'
 
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.menuStore = new MenuStore();
-    window['menuStore'] = this.menuStore;
+import ModalDialog from './modal-dialog'
+import SubscribeDialog from './dialogs/subscribe-dialog'
+import SubscribeSocialMediaDialog from './dialogs/subscribe-social-media-dialog'
+import SubscribeAppDialog from './dialogs/subscribe-app-dialog'
+import SubscribeBulletinDialog from './dialogs/subscribe-bulletin-dialog'
+import SubscribeBlogDialog from './dialogs/subscribe-blog-dialog'
+import UnsupportedBrowserDialog from './../components/dialogs/unsupported-browser-dialog'
+import CookieConsent from './dialogs/cookie-consent'
+
+import { renderRoutes } from 'react-router-config'
+import { modal_init } from '../js/modal'
+import { tooltip_init } from '../js/tooltip'
+import { navigation_init } from '../js/navigation'
+import { video_init } from '../js/video'
+
+@observer class Page extends React.Component {
+  constructor (props) {
+    super(props)
+    this.menuStore = new MenuStore()
+    window['menuStore'] = this.menuStore
   }
 
-  componentDidUpdate() {
-    this._didUpdate();
+  componentDidUpdate () {
+    this._didUpdate()
   }
 
-  componentDidMount() {
-    this._didUpdate();
+  componentDidMount () {
+    this._didUpdate()
   }
 
-  componentWillReceiveProps(nextProps) {
-    if(nextProps.location !== this.props.location
-      && nextProps.history.action === 'PUSH'
-      && !nextProps.location.hash) {
-
+  componentWillReceiveProps (nextProps) {
+    if (
+      nextProps.location !== this.props.location &&
+      nextProps.history.action === 'PUSH' &&
+      !nextProps.location.hash
+    ) {
       // scroll to top on forward page change (if no hash is set)
       // see https://github.com/ReactTraining/react-router/issues/2019
-      window.scrollTo(0, 0);
+      window.scrollTo(0, 0)
     }
   }
 
-  _didUpdate() {
-    if (
-      this.props.location.pathname === '' ||
-      this.props.location.pathname === '/'
-    ) {
-      this.props.history.push('bulletin');
+  _didUpdate () {
+    if (this.props.location.pathname === '' || this.props.location.pathname === '/') {
+      this.props.history.push('bulletin')
     }
-    modal_init();
-    tooltip_init();
-    navigation_init();
-    video_init();
+    modal_init()
+    tooltip_init()
+    navigation_init()
+    video_init()
   }
 
-  render() {
+  render () {
     return (
       <div>
         <PageLoadingScreen />
         <Jumpnav />
-        <div id="page-all" className="page-all">
+        <div id='page-all' className='page-all'>
           <PageHeader menuStore={this.menuStore} />
-          <main id="page-main" className="page-main">
-            <div id="global-grid">{renderRoutes(this.props.route.routes)}</div>
+          <main id='page-main' className='page-main'>
+            <div id='global-grid'>{renderRoutes(this.props.route.routes)}</div>
           </main>
           <PageFooter menuStore={this.menuStore} />
         </div>
-        <ModalDialog id="subscribeDialog">
+        {appStore.unsupportedBrowserModal && <UnsupportedBrowserDialog />}
+        <ModalDialog id='subscribeDialog'>
           <SubscribeDialog />
         </ModalDialog>
-        <ModalDialog id="subscribeBulletinDialog">
+        <ModalDialog id='subscribeBulletinDialog'>
           <SubscribeBulletinDialog />
         </ModalDialog>
-        <ModalDialog id="subscribeBlogDialog">
+        <ModalDialog id='subscribeBlogDialog'>
           <SubscribeBlogDialog />
         </ModalDialog>
-        <ModalDialog id="subscribeSocialMediaDialog">
+        <ModalDialog id='subscribeSocialMediaDialog'>
           <SubscribeSocialMediaDialog />
         </ModalDialog>
-        <ModalDialog id="subscribeAppDialog">
+        <ModalDialog id='subscribeAppDialog'>
           <SubscribeAppDialog />
         </ModalDialog>
         <CookieConsent />
       </div>
-    );
+    )
   }
 }
 
-export default withRouter(Page);
+export default withRouter(Page)
