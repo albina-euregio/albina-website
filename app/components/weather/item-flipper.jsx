30bea429e994058eab69aa38999591d409454722
adammertel <mertel.adam@gmail.com>
2018-09-24 13:23:53 +0200

    flipper arrows alignment

--- app/components/weather/item-flipper.jsx
+++ app/components/weather/item-flipper.jsx
@@ -3,22 +3,22 @@ import { observer, inject } from 'mobx-react'
 import { injectIntl } from 'react-intl'
 
 class ItemFlipper extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
   }
 
-  render () {
+  render() {
     const lang = appStore.language
     const previousItem = this.props.store.previousItem
     const nextItem = this.props.store.nextItem
 
     return (
-      <div className='grid flipper-left-right'>
-        <div className='all-6 grid-item'>
-          {previousItem &&
+      <div className="grid flipper-left-right">
+        <div className="all-6 grid-item">
+          {previousItem && (
             <a
-              href='#'
-              className='icon-link tooltip flipper-left'
+              href="#"
+              className="icon-link tooltip flipper-left"
               /* title={this.props.intl.formatMessage({
                 id: 'weathermap:header:dateflipper:back'
               })} */
@@ -28,15 +28,17 @@ class ItemFlipper extends React.Component {
                 this.props.handleChange(previousItem.id)
               }}
             >
-              <span className='icon-arrow-left' />
-              &nbsp;{previousItem.descriptionTimeSpan[lang]}
-            </a>}
+              <span className="icon-arrow icon-arrow-left" />
+              &nbsp;
+              {previousItem.descriptionTimeSpan[lang]}
+            </a>
+          )}
         </div>
-        <div className='all-6 grid-item'>
-          {nextItem &&
+        <div className="all-6 grid-item">
+          {nextItem && (
             <a
-              href='#'
-              className='icon-link tooltip flipper-left'
+              href="#"
+              className="icon-link tooltip flipper-left"
               /* title={this.props.intl.formatMessage({
                 id: 'weathermap:header:dateflipper:forward'
               })} */
@@ -46,9 +48,11 @@ class ItemFlipper extends React.Component {
                 this.props.handleChange(nextItem.id)
               }}
             >
-              {nextItem.descriptionTimeSpan[lang]}&nbsp;
-              <span className='icon-arrow-right' />
-            </a>}
+              {nextItem.descriptionTimeSpan[lang]}
+              &nbsp;
+              <span className="icon-arrow icon-arrow-right" />
+            </a>
+          )}
         </div>
       </div>
     )
