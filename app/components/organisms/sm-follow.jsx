e0246707f2e220a8f9ebf5d6ae3ca64cad63bea5
adammertel <mertel.adam@gmail.com>
2018-11-15 16:01:38 +0100

    follow us - only used social medias

--- app/components/organisms/sm-follow.jsx
+++ app/components/organisms/sm-follow.jsx
@@ -4,7 +4,11 @@ import { injectIntl, FormattedHTMLMessage } from 'react-intl'
 
 class SmFollow extends React.Component {
   render () {
-    const accounts = config.get('subscribe.socialMedia')
+    const accounts = config.get('subscribe.socialMedia').filter(account => {
+      return Object.keys(account.url).some(region => {
+        return account.url[region]
+      })
+    })
     return (
       <section className='section section-padding sm-share-follow'>
         <p><FormattedHTMLMessage id='footer:follow-us' /></p>
