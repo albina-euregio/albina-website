8d0d1eb3ea913a524175a3cc3b7078e43ea4d6b0
adammertel <mertel.adam@gmail.com>
2018-11-21 17:50:31 +0100

    blog middleware stats endpoint

--- server/blog.js
+++ server/blog.js
@@ -7,9 +7,39 @@ const cors = require('cors')
 var bloggerApiPath = 'https://www.googleapis.com/blogger/v3/blogs'
 var storedBlogs = []
 
+var stats = {
+  requested: 0,
+  proccessed: 0
+}
+
 app.use(cors())
 
+app.get('/stats', (req, res) => {
+  res.send(
+    '<!doctype html><body>' +
+      '<dl>' +
+      '<div><span>requested: </span><strong>' +
+      stats.requested +
+      '</strong></div>' +
+      '<div><span>proccessed: </span><strong>' +
+      stats.proccessed +
+      '</strong></div>' +
+      '<div><span>saved requests: </span><strong>' +
+      (stats.requested ? 100 - stats.proccessed / stats.requested * 100 : '-') +
+      '</strong></div>' +
+      '</dl>' +
+      '<div>stored urls: </div>' +
+      storedBlogs
+        .map(blog => {
+          return '<p>' + blog.url + '</p>'
+        })
+        .join('') +
+      '</html>'
+  )
+})
+
 app.get('/:id/*', (req, res) => {
+  stats.requested += 1
   const url = req.url
   console.log('!!getting a request', bloggerApiPath + url)
 
@@ -21,28 +51,32 @@ app.get('/:id/*', (req, res) => {
     console.log('stored before')
     res.send(stored.response)
   } else {
+    stats.proccessed += 1
     console.log('not stored before, have to be loaded')
-    request({ url: bloggerApiPath + url }, (err, response, body) => {
-      // console.log('response', response)
-      console.log('loaded, sending back')
-      if (!err) {
-        const blog = {
-          url: url,
-          response: JSON.parse(body),
-          time: now.valueOf()
-        }
-        storedBlogs.push(blog)
+    request(
+      { url: bloggerApiPath + url + '&maxResults=100' },
+      (err, response, body) => {
+        // console.log('response', response)
+        console.log('loaded, sending back')
+        if (!err) {
+          const blog = {
+            url: url,
+            response: JSON.parse(body),
+            time: now.valueOf()
+          }
+          storedBlogs.push(blog)
 
-        res.send(JSON.parse(body))
-      } else {
-        console.log('error', err)
-        res.send(false)
+          res.send(JSON.parse(body))
+        } else {
+          console.log('error while loading', err)
+          res.send(false)
+        }
       }
-    })
+    )
   }
 })
 
-const cleaningInterval = 5000
+const cleaningInterval = 50000
 
 // clean the old requests
 setInterval(() => {
