8d0fa1ab7144ac2b18472e727129f09aace8b3f9
adammertel <mertel.adam@gmail.com>
2018-11-26 12:07:27 +0100

    hiding subscribe button

--- app/components/bulletin/bulletin-buttonbar.jsx
+++ app/components/bulletin/bulletin-buttonbar.jsx
@@ -1,68 +1,75 @@
-import React from 'react'
-import { inject } from 'mobx-react'
-import { injectIntl, FormattedHTMLMessage } from 'react-intl'
-import stringInject from 'stringinject'
+import React from "react";
+import { inject } from "mobx-react";
+import { injectIntl, FormattedHTMLMessage } from "react-intl";
+import stringInject from "stringinject";
 
 class BulletinButtonbar extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
   }
 
-  render () {
+  render() {
     return (
       <section
-        id='section-bulletin-buttonbar'
-        className='section-padding section-bulletin-buttonbar'>
-        <div className='section-centered'>
-          <div className='grid linkbar'>
-            <div className='normal-4 grid-item'>
+        id="section-bulletin-buttonbar"
+        className="section-padding section-bulletin-buttonbar"
+      >
+        <div className="section-centered">
+          <div className="grid linkbar">
+            <div className="normal-4 grid-item">
               <a
-                href='#page-main'
+                href="#page-main"
                 title={this.props.intl.formatMessage({
-                  id: 'bulletin:linkbar:back-to-map:hover'
+                  id: "bulletin:linkbar:back-to-map:hover"
                 })}
-                className='icon-link icon-arrow-up tooltip'
-                data-scroll=''>
-                <FormattedHTMLMessage id='bulletin:linkbar:back-to-map' />
+                className="icon-link icon-arrow-up tooltip"
+                data-scroll=""
+              >
+                <FormattedHTMLMessage id="bulletin:linkbar:back-to-map" />
               </a>
             </div>
-            <div className='normal-8 grid-item'>
-              <ul className='list-inline bulletin-buttonbar'>
-                {this.props.store.activeBulletinCollection &&
+            <div className="normal-8 grid-item">
+              <ul className="list-inline bulletin-buttonbar">
+                {this.props.store.activeBulletinCollection && (
                   <li>
                     <a
-                      href={stringInject(config.get('links.downloads.pdf'), {
+                      href={stringInject(config.get("links.downloads.pdf"), {
                         date: this.props.store.settings.date,
-                        lang: window['appStore'].language
+                        lang: window["appStore"].language
                       })}
-                      target='_blank'
+                      target="_blank"
                       title={this.props.intl.formatMessage({
-                        id: 'bulletin:linkbar:pdf:hover'
+                        id: "bulletin:linkbar:pdf:hover"
                       })}
-                      className='pure-button tooltip'>
+                      className="pure-button tooltip"
+                    >
                       {this.props.intl.formatMessage({
-                        id: 'bulletin:linkbar:pdf'
+                        id: "bulletin:linkbar:pdf"
                       })}
                     </a>
-                  </li>}
-                <li>
-                  <a
-                    href='#subscribeDialog'
-                    title={this.props.intl.formatMessage({
-                      id: 'bulletin:linkbar:subscribe:hover'
-                    })}
-                    className='modal-trigger popup-modal pure-button tooltip'>
-                    {this.props.intl.formatMessage({
-                      id: 'bulletin:linkbar:subscribe'
-                    })}
-                  </a>
-                </li>
+                  </li>
+                )}
+                {!config.get("subscribe.buttonHidden") && (
+                  <li>
+                    <a
+                      href="#subscribeDialog"
+                      title={this.props.intl.formatMessage({
+                        id: "bulletin:linkbar:subscribe:hover"
+                      })}
+                      className="modal-trigger popup-modal pure-button tooltip"
+                    >
+                      {this.props.intl.formatMessage({
+                        id: "bulletin:linkbar:subscribe"
+                      })}
+                    </a>
+                  </li>
+                )}
               </ul>
             </div>
           </div>
         </div>
       </section>
-    )
+    );
   }
 }
-export default inject('locale')(injectIntl(BulletinButtonbar))
+export default inject("locale")(injectIntl(BulletinButtonbar));
--- app/components/organisms/page-footer.jsx
+++ app/components/organisms/page-footer.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { observer, inject } from 'mobx-react'
-import { injectIntl } from 'react-intl'
-import Menu from '../menu'
-import SmFollow from './sm-follow.jsx'
+import React from "react";
+import { observer, inject } from "mobx-react";
+import { injectIntl } from "react-intl";
+import Menu from "../menu";
+import SmFollow from "./sm-follow.jsx";
 import {
   parseRawHtml,
   defaultProcessor,
   replaceInternalLinksProcessor
-} from '../../util/htmlParser'
-import stringInject from 'stringinject'
+} from "../../util/htmlParser";
+import stringInject from "stringinject";
 
 class PageFooter extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { content: '' }
+  constructor(props) {
+    super(props);
+    this.state = { content: "" };
   }
 
-  componentDidMount () {
-    window['staticPageStore'].loadBlock('footer_text').then(content => {
-      const parsed = JSON.parse(content)
+  componentDidMount() {
+    window["staticPageStore"].loadBlock("footer_text").then(content => {
+      const parsed = JSON.parse(content);
       if (parsed.data && parsed.data.length > 0) {
-        const el = parsed.data[0]
+        const el = parsed.data[0];
         if (
           el &&
           el.attributes &&
@@ -29,72 +29,77 @@ class PageFooter extends React.Component {
         ) {
           this.setState({
             content: this._preprocessContent(el.attributes.body.processed)
-          })
+          });
         }
       }
-    })
+    });
   }
 
-  _preprocessContent (content) {
-    const instructions = [replaceInternalLinksProcessor(), defaultProcessor()]
-    return parseRawHtml(content, instructions)
+  _preprocessContent(content) {
+    const instructions = [replaceInternalLinksProcessor(), defaultProcessor()];
+    return parseRawHtml(content, instructions);
   }
 
-  render () {
-    const footerMenuMore = this.props.menuStore.getMenu('footer')
-    const footerMenuMain = this.props.menuStore.getMenu('footer-main')
+  render() {
+    const footerMenuMore = this.props.menuStore.getMenu("footer");
+    const footerMenuMain = this.props.menuStore.getMenu("footer-main");
 
     return (
-      <div id='page-footer' className='page-footer'>
-        <section className='section section-padding page-footer-navigation'>
-          <div className='grid'>
-            <div className='grid-item normal-6'>
+      <div id="page-footer" className="page-footer">
+        <section className="section section-padding page-footer-navigation">
+          <div className="grid">
+            <div className="grid-item normal-6">
               <Menu
-                className='list-inline footer-navigation footer-navigation-more'
+                className="list-inline footer-navigation footer-navigation-more"
                 entries={footerMenuMore}
               />
               <Menu
-                className='list-plain footer-navigation footer-navigation-main'
+                className="list-plain footer-navigation footer-navigation-main"
                 entries={footerMenuMain}
               />
             </div>
-            <div className='grid-item normal-6'>
-              <p className='page-footer-subscribe'>
+            <div className="grid-item normal-6">
+              {!config.get("subscribe.buttonHidden") && (
+                <p className="page-footer-subscribe">
+                  <a
+                    href="#subscribeDialog"
+                    title={this.props.intl.formatMessage({
+                      id: "footer:subscribe:hover"
+                    })}
+                    className="modal-trigger popup-modal pure-button tooltip"
+                  >
+                    {this.props.intl.formatMessage({
+                      id: "footer:subscribe"
+                    })}
+                  </a>
+                </p>
+              )}
+              <p className="page-footer-text">{this.state.content}</p>
+              <p className="page-footer-interreg">
                 <a
-                  href='#subscribeDialog'
+                  href={stringInject(config.get("links.interreg"), {
+                    lang: window["appStore"].language
+                  })}
+                  className="logo-interreg tooltip"
                   title={this.props.intl.formatMessage({
-                    id: 'footer:subscribe:hover'
+                    id: "footer:interreg:hover"
                   })}
-                  className='modal-trigger popup-modal pure-button tooltip'>
-                  {this.props.intl.formatMessage({
-                    id: 'footer:subscribe'
-                  })}
-                </a>
-              </p>
-              <p className='page-footer-text'>{this.state.content}</p>
-              <p className='page-footer-interreg'>
-                <a
-                  href={stringInject(config.get('links.interreg'), {
-                    lang: window['appStore'].language
-                  })}
-                  className='logo-interreg tooltip'
-                  title={this.props.intl.formatMessage({
-                    id: 'footer:interreg:hover'
-                  })}
-                  target='_blank'>
+                  target="_blank"
+                >
                   <span>Interreg</span>
                 </a>
               </p>
             </div>
-            <div className='grid-item all-12'>
-              <p className='page-footer-top'>
+            <div className="grid-item all-12">
+              <p className="page-footer-top">
                 <a
-                  href='#page-main'
-                  className='icon-arrow-up tooltip'
+                  href="#page-main"
+                  className="icon-arrow-up tooltip"
                   title={this.props.intl.formatMessage({
-                    id: 'footer:top:hover'
+                    id: "footer:top:hover"
                   })}
-                  data-scroll=''>
+                  data-scroll=""
+                >
                   <span>Top</span>
                 </a>
               </p>
@@ -113,7 +118,7 @@ class PageFooter extends React.Component {
         </section>
         <SmFollow />
       </div>
-    )
+    );
   }
 }
-export default inject('locale')(injectIntl(observer(PageFooter)))
+export default inject("locale")(injectIntl(observer(PageFooter)));
