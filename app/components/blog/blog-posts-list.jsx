f2110c0cd1a312d25f6881d496ec7235a5c95150
adammertel <mertel.adam@gmail.com>
2018-11-26 14:17:34 +0100

    setting actual date to the blog filter

--- app/components/blog/blog-posts-list.jsx
+++ app/components/blog/blog-posts-list.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { observer } from 'mobx-react'
-import TagList from './tag-list'
-import { dateToDateTimeString } from '../../util/date.js'
+import React from "react";
+import { Link } from "react-router-dom";
+import { observer } from "mobx-react";
+import TagList from "./tag-list";
+import { dateToDateTimeString } from "../../util/date.js";
 
-@observer class BlogPostsList extends React.Component {
-  constructor (props) {
-    super(props)
+@observer
+class BlogPostsList extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
-  render () {
+  render() {
     return (
       <div>
         {!this.props.loading &&
@@ -17,37 +18,34 @@ import { dateToDateTimeString } from '../../util/date.js'
             return (
               <Link
                 key={i}
-                to={'/blog/' + item.blogName + '/' + item.postId}
-                className='linkbox linkbox-blog-feature'>
-                <div className='content-image'>
+                to={"/blog/" + item.blogName + "/" + item.postId}
+                className="linkbox linkbox-blog-feature"
+              >
+                <div className="content-image">
                   {item.image && <img src={item.image} />}
                 </div>
-                <div className='content-text'>
-                  <ul className='list-inline blog-feature-meta'>
-                    <li className='blog-author'>{item.author}</li>
-                    <li className='blog-date'>
+                <div className="content-text">
+                  <ul className="list-inline blog-feature-meta">
+                    <li className="blog-author">{item.author}</li>
+                    <li className="blog-date">
                       {dateToDateTimeString(item.date)}
                     </li>
-                    <li className='blog-province'>
+                    <li className="blog-province">
                       {item.regions
-                        .map(r => window['appStore'].getRegionName(r))
-                        .join(', ')}
+                        .map(r => window["appStore"].getRegionName(r))
+                        .join(", ")}
                     </li>
-                    <li className='blog-language'>
-                      {item.lang.toUpperCase()}
-                    </li>
+                    <li className="blog-language">{item.lang.toUpperCase()}</li>
                   </ul>
-                  <h1 className='subheader blog-feature-title'>
-                    {item.title}
-                  </h1>
+                  <h1 className="subheader blog-feature-title">{item.title}</h1>
                   <TagList tags={item.tags} />
                 </div>
               </Link>
-            )
+            );
           })}
       </div>
-    )
+    );
   }
 }
 
-export default BlogPostsList
+export default BlogPostsList;
--- app/stores/blogStore.js
+++ app/stores/blogStore.js
@@ -1,10 +1,10 @@
-import { observable, action, computed, toJS } from 'mobx'
-import Base from '../base'
-import { parseDate, getDaysOfMonth } from '../util/date'
-import { parseTags } from '../util/tagging'
+import { observable, action, computed, toJS } from "mobx";
+import Base from "../base";
+import { parseDate, getDaysOfMonth } from "../util/date";
+import { parseTags } from "../util/tagging";
 
 class BlogPostPreviewItem {
-  constructor (
+  constructor(
     blogName,
     postId,
     url,
@@ -16,101 +16,106 @@ class BlogPostPreviewItem {
     image = null,
     tags = []
   ) {
-    this.blogName = blogName
-    this.postId = postId
-    this.url = url
-    this.author = author
-    this.date = date
-    this.title = title
-    this.lang = lang
-    this.regions = regions
-    this.image = image
-    this.tags = tags
+    this.blogName = blogName;
+    this.postId = postId;
+    this.url = url;
+    this.author = author;
+    this.date = date;
+    this.title = title;
+    this.lang = lang;
+    this.regions = regions;
+    this.image = image;
+    this.tags = tags;
   }
 }
 
 export default class BlogStore {
-  regions
-  languages
-  _year
-  _month
-  _avalancheProblem
-  _searchText
-  _loading
-  _posts
+  regions;
+  languages;
+  _year;
+  _month;
+  _avalancheProblem;
+  _searchText;
+  _loading;
+  _posts;
 
-  constructor () {
+  constructor() {
     // get all regions from appStore and activate them
-    const rs = Object.keys(window['appStore'].regions).reduce((acc, r) => {
-      acc[r] = { active: true }
-      return acc
-    }, {})
-    this.regions = observable(rs)
+    const rs = Object.keys(window["appStore"].regions).reduce((acc, r) => {
+      acc[r] = { active: true };
+      return acc;
+    }, {});
+    this.regions = observable(rs);
 
     this.languages = observable({
       de: { active: true },
       it: { active: true },
       en: { active: true }
-    })
+    });
 
     // Do not make posts observable, otherwise posts list will be
     // unnecessaryliy rerendered during the filling of this array.
     // Views should only observe the value of the "loading" flag instead.
-    this._posts = {}
+    this._posts = {};
 
-    this._year = observable.box('')
-    this._month = observable.box('')
-    this._avalancheProblem = observable.box('')
+    this._year = observable.box("");
+    this._month = observable.box("");
+    this._avalancheProblem = observable.box("");
+
+    // setting the actual date
+    const date = new Date();
+    this._year.set(date.getFullYear());
+    this._month.set(date.getMonth() + 1);
 
     // For Mobx > v4 we have to use an obserable box instead of
     // @observable loading = ...;
-    this._loading = observable.box(false)
-    this._searchText = observable.box('')
+    this._loading = observable.box(false);
+    this._searchText = observable.box("");
 
     this.blogProcessor = {
       blogger: {
         createUrl: config => {
           const baseUrl =
-            window['config'].get('apis.blogger') + config.params.id + '/posts'
+            window["config"].get("apis.blogger") + config.params.id + "/posts";
 
           if (this.searchText) {
             // create search URL
             const params = {
-              key: window['config'].get('apiKeys.google'),
+              key: window["config"].get("apiKeys.google"),
               q: this.searchText
-            }
+            };
 
-            return Base.makeUrl(baseUrl + '/search', params)
+            return Base.makeUrl(baseUrl + "/search", params);
           }
 
           // else
           const params = {
-            key: window['config'].get('apiKeys.google'),
+            key: window["config"].get("apiKeys.google"),
             fetchBodies: false,
             fetchImages: true,
-            status: 'live'
-          }
+            status: "live"
+          };
           if (this.avalancheProblem) {
-            params['labels'] = this.avalancheProblem
+            params["labels"] = this.avalancheProblem;
           }
           if (this.year) {
-            params['startDate'] = this._startDate.toISOString()
-            params['endDate'] = this._endDate.toISOString()
+            params["startDate"] = this._startDate.toISOString();
+            params["endDate"] = this._endDate.toISOString();
           }
           // TODO search
 
           return Base.makeUrl(
-            window['config'].get('apis.blogger') + config.params.id + '/posts',
+            window["config"].get("apis.blogger") + config.params.id + "/posts",
             params
-          )
+          );
         },
         process: (response, config) => {
           if (Array.isArray(response.items)) {
             return response.items.map(item => {
-              const previewImage = Array.isArray(item.images) &&
-                item.images.length > 0
-                ? item.images[0].url
-                : null
+              const previewImage =
+                Array.isArray(item.images) && item.images.length > 0
+                  ? item.images[0].url
+                  : null;
 
               return new BlogPostPreviewItem(
                 config.name,
@@ -123,58 +128,58 @@ export default class BlogStore {
                 config.regions,
                 previewImage,
                 parseTags(item.labels)
-              )
-            })
+              );
+            });
           }
-          return []
+          return [];
         }
       }
-    }
+    };
   }
 
-  @action load (forceReload = false) {
+  @action load(forceReload = false) {
     if (!forceReload && this._posts.length > 0) {
       // don't do a reload if already loaded unless reload is forced
-      return
+      return;
     }
 
-    this.loading = true
+    this.loading = true;
 
-    const blogsConfig = window['config'].get('blogs')
-    const loads = []
+    const blogsConfig = window["config"].get("blogs");
+    const loads = [];
 
-    const newPosts = {}
+    const newPosts = {};
 
     // filter config for lang and region
     for (let cfg of blogsConfig) {
-      newPosts[cfg.name] = []
+      newPosts[cfg.name] = [];
 
       if (this.languages[cfg.lang] && this.languages[cfg.lang].active) {
         if (cfg.regions.some(r => this.regions[r] && this.regions[r].active)) {
           if (this.blogProcessor[cfg.apiType]) {
-            const p = this.blogProcessor[cfg.apiType]
+            const p = this.blogProcessor[cfg.apiType];
 
-            const url = p.createUrl(cfg)
-            console.log(url)
+            const url = p.createUrl(cfg);
+            console.log(url);
             loads.push(
               Base.doRequest(url).then(
                 response => {
-                  console.log(response)
+                  console.log(response);
                   p.process(JSON.parse(response), cfg).forEach(i => {
-                    newPosts[cfg.name].push(i)
-                  })
+                    newPosts[cfg.name].push(i);
+                  });
                 },
                 (errorText, statusCode) => {
-                  console.log(errorText)
+                  console.log(errorText);
                   if (
                     parseInt(statusCode) == 304 &&
                     Array.isArray(this._posts[cfg.name])
                   ) {
-                    newPosts[cfg.name] = this._posts[cfg.name]
+                    newPosts[cfg.name] = this._posts[cfg.name];
                   }
                 }
               )
-            )
+            );
           }
         }
       }
@@ -182,78 +187,78 @@ export default class BlogStore {
 
     return Promise.all(loads).then(() => {
       Object.keys(newPosts).forEach(queue => {
-        this._posts[queue] = newPosts[queue]
-      })
-      this.loading = false
-    })
+        this._posts[queue] = newPosts[queue];
+      });
+      this.loading = false;
+    });
   }
 
-  get loading () {
-    return this._loading.get()
+  get loading() {
+    return this._loading.get();
   }
 
-  set loading (val) {
-    this._loading.set(val)
+  set loading(val) {
+    this._loading.set(val);
   }
 
-  get searchText () {
-    return this._searchText.get()
+  get searchText() {
+    return this._searchText.get();
   }
 
-  set searchText (val) {
+  set searchText(val) {
     if (val != this._searchText.get()) {
-      this._searchText.set(val)
-      this.load(true)
+      this._searchText.set(val);
+      this.load(true);
     }
   }
 
-  get avalancheProblem () {
-    return this._avalancheProblem.get()
+  get avalancheProblem() {
+    return this._avalancheProblem.get();
   }
 
-  set avalancheProblem (val) {
-    this._avalancheProblem.set(val)
-    this.load(true)
+  set avalancheProblem(val) {
+    this._avalancheProblem.set(val);
+    this.load(true);
   }
 
-  @computed get languageFilter () {
-    const languages = toJS(this.languages)
+  @computed get languageFilter() {
+    const languages = toJS(this.languages);
     const activeLanguages = Object.keys(languages).filter(
       lang => languages[lang].active
-    )
+    );
 
-    return activeLanguages.length > 1 ? 'all' : activeLanguages[0]
+    return activeLanguages.length > 1 ? "all" : activeLanguages[0];
   }
 
-  get year () {
-    return this._year.get()
+  get year() {
+    return this._year.get();
   }
 
-  set year (y) {
-    this._year.set(y)
-    this.load(true)
+  set year(y) {
+    this._year.set(y);
+    this.load(true);
   }
 
-  get month () {
-    return this._month.get()
+  get month() {
+    return this._month.get();
   }
 
-  set month (m) {
-    this._month.set(m)
-    this.load(true)
+  set month(m) {
+    this._month.set(m);
+    this.load(true);
   }
 
-  get _startDate () {
+  get _startDate() {
     if (this.year) {
       if (this.month) {
-        return new Date(this.year, this.month - 1, 1)
+        return new Date(this.year, this.month - 1, 1);
       }
-      return new Date(this.year, 0, 1)
+      return new Date(this.year, 0, 1);
     }
-    return null
+    return null;
   }
 
-  get _endDate () {
+  get _endDate() {
     if (this.year) {
       if (this.month) {
         return new Date(
@@ -262,81 +267,81 @@ export default class BlogStore {
           getDaysOfMonth(this.year, this.month),
           23,
           59
-        )
+        );
       }
-      return new Date(this.year, 11, 31, 23, 59)
+      return new Date(this.year, 11, 31, 23, 59);
     }
-    return null
+    return null;
   }
 
-  @action setRegionFilter (region) {
+  @action setRegionFilter(region) {
     for (let r in this.regions) {
-      this.regions[r].active = !region || r === region
+      this.regions[r].active = !region || r === region;
     }
-    this.load(true)
+    this.load(true);
   }
 
-  @action setLanguageFilter (lang) {
+  @action setLanguageFilter(lang) {
     for (let l in this.languages) {
-      this.languages[l].active = !lang || l === lang
+      this.languages[l].active = !lang || l === lang;
     }
-    this.load(true)
+    this.load(true);
   }
 
-  getPosts (start = 0, limit = 50) {
+  getPosts(start = 0, limit = 10) {
     const totalLength = Object.values(this._posts)
       .map(l => l.length)
-      .reduce((acc, v) => acc + v, 0)
+      .reduce((acc, v) => acc + v, 0);
 
-    const queues = Object.keys(this._posts)
+    const queues = Object.keys(this._posts);
 
-    const startIndex = Math.min(start, totalLength - 1)
-    const end = Math.min(start + limit, totalLength)
-    const postPointer = {}
+    const startIndex = Math.min(start, totalLength - 1);
+    const end = Math.min(start + limit, totalLength);
+    const postPointer = {};
     queues.forEach(queue => {
-      postPointer[queue] = 0
-    })
+      postPointer[queue] = 0;
+    });
 
     const getNext = () => {
       // get the next items of all queues
-      const candidates = {}
+      const candidates = {};
       queues.forEach(queue => {
         if (this._posts[queue].length > postPointer[queue]) {
-          candidates[queue] = this._posts[queue][postPointer[queue]]
+          candidates[queue] = this._posts[queue][postPointer[queue]];
         }
-      })
+      });
 
       // find the maximum
       const queueMax = Object.keys(candidates).reduce((acc, queue) => {
         if (!acc || candidates[queue].date > candidates[acc].date) {
-          return queue
+          return queue;
         }
-        return acc
-      }, '')
+        return acc;
+      }, "");
 
       if (queueMax) {
-        const next = this._posts[queueMax][postPointer[queueMax]]
-        postPointer[queueMax]++
-        return next
+        const next = this._posts[queueMax][postPointer[queueMax]];
+        postPointer[queueMax]++;
+        return next;
       }
-      return null
-    }
+      return null;
+    };
 
-    const list = []
+    const list = [];
     if (startIndex >= 0) {
       for (let i = 0; i < startIndex; i++) {
-        getNext()
+        getNext();
       }
 
       for (let j = startIndex; j < end; j++) {
-        const n = getNext()
+        const n = getNext();
         if (n) {
-          list.push(n)
+          list.push(n);
         }
       }
 
-      return list
+      return list;
     }
-    return []
+    return [];
   }
 }
