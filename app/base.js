e2883174d0510a2bbfa986bf883d0581a0e8c8d5
adammertel <mertel.adam@gmail.com>
2018-11-27 17:08:03 +0100

    search change method refactor

--- app/base.js
+++ app/base.js
@@ -1,114 +1,130 @@
-require('url-search-params-polyfill')
+require("url-search-params-polyfill");
 
 var Base = {
-  makeUrl (baseUrl, params = {}) {
+  makeUrl(baseUrl, params = {}) {
     return (
       baseUrl +
       (Object.keys(params).length > 0
-        ? '?' +
-            Object.keys(params)
-              .map(k => {
-                return k + '=' + encodeURIComponent(params[k])
-              })
-              .join('&')
-        : '')
-    )
+        ? "?" +
+          Object.keys(params)
+            .map(k => {
+              return k + "=" + encodeURIComponent(params[k]);
+            })
+            .join("&")
+        : "")
+    );
   },
 
-  doRequest (url, type = 'json') {
-    return new Promise(function (resolve, reject) {
-      let xhr = new XMLHttpRequest()
-      xhr.onreadystatechange = function () {
+  doRequest(url, type = "json") {
+    return new Promise(function(resolve, reject) {
+      let xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
         if (xhr.readyState == XMLHttpRequest.DONE) {
           if (xhr.status == 200) {
-            resolve(xhr.responseText)
+            resolve(xhr.responseText);
           } else {
-            reject(xhr.statusText, xhr.status)
+            reject(xhr.statusText, xhr.status);
           }
         }
-      }
-      xhr.open('GET', url, true)
+      };
+      xhr.open("GET", url, true);
 
       // set content type
-      if (type === 'json') {
+      if (type === "json") {
         xhr.setRequestHeader(
-          'Accept',
-          'application/json,application/vnd.application+json,application/vnd.api+json'
-        )
+          "Accept",
+          "application/json,application/vnd.application+json,application/vnd.api+json"
+        );
       }
-      xhr.send(null)
-    })
+      xhr.send(null);
+    });
   },
 
-  doPost (url, payload, type = 'json') {
-    return new Promise(function (resolve, reject) {
-      console.log('post: ' + url + ' ' + JSON.stringify(payload))
-      let xhr = new XMLHttpRequest()
-      xhr.onreadystatechange = function () {
+  doPost(url, payload, type = "json") {
+    return new Promise(function(resolve, reject) {
+      console.log("post: " + url + " " + JSON.stringify(payload));
+      let xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
         if (xhr.readyState == XMLHttpRequest.DONE) {
           if (xhr.status == 200) {
-            resolve(xhr.responseText)
+            resolve(xhr.responseText);
           } else {
-            reject(xhr.statusText, xhr.status)
+            reject(xhr.statusText, xhr.status);
           }
         }
-      }
-      xhr.open('POST', url, true)
+      };
+      xhr.open("POST", url, true);
 
       // set content type
-      if (type === 'json') {
-        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+      if (type === "json") {
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         xhr.setRequestHeader(
-          'Accept',
-          'application/json,application/vnd.application+json,application/vnd.api+json'
-        )
+          "Accept",
+          "application/json,application/vnd.application+json,application/vnd.api+json"
+        );
       }
-      xhr.send(JSON.stringify(payload))
-    })
+      xhr.send(JSON.stringify(payload));
+    });
   },
 
-  cleanCache (fileName) {
+  cleanCache(fileName) {
     if (window.caches) {
-      window.caches.delete(fileName)
+      window.caches.delete(fileName);
     }
   },
 
-  checkBlendingSupport () {
-    const bodyEl = document.getElementsByTagName('body')[0]
-    const bodyElStyle = window.getComputedStyle(bodyEl)
-    const blendMode = bodyElStyle.getPropertyValue('mix-blend-mode')
-    return !!blendMode
+  checkBlendingSupport() {
+    const bodyEl = document.getElementsByTagName("body")[0];
+    const bodyElStyle = window.getComputedStyle(bodyEl);
+    const blendMode = bodyElStyle.getPropertyValue("mix-blend-mode");
+    return !!blendMode;
   },
 
-  searchGet (variable) {
-    const search = new URLSearchParams(document.location.search.substring(1))
-    return search.get(variable)
+  searchGet(variable, search = false) {
+    const searchParams = search ? search : this.makeSearch();
+    return searchParams.get(variable);
   },
 
-  searchChange (history, variable, value, replace = false) {
-    let search = new URLSearchParams(document.location.search.substring(1))
-    const actualValue = this.searchGet(variable)
+  makeSearch() {
+    return new URLSearchParams(document.location.search.substring(1));
+  },
 
-    if (actualValue !== false) {
-      if (actualValue !== value) {
-        search.set(variable, value)
-      } else {
-        search = false
+  searchChange(history, params, replace = false) {
+    const search = this.makeSearch();
+
+    // checking, what has changed
+    const changes = {};
+    Object.keys(params).forEach(paramKey => {
+      const newValue = params[paramKey] ? params[paramKey].toString() : "";
+      const oldValue = search.get(paramKey);
+      if (oldValue !== newValue) {
+        changes[paramKey] = newValue;
       }
-    } else {
-      search.append(variable, value)
-    }
-    if (search && history) {
-      if (actualValue !== value) {
-        if (replace) {
-          history.replace({ search: search.toString() })
+    });
+
+    console.log("search changes", JSON.stringify(changes));
+
+    if (history && search && Object.keys(changes).length > 0) {
+      // setting new search object
+      Object.keys(changes).forEach(changeKey => {
+        const changeValue = changes[changeKey];
+        if (search.get(changeKey)) {
+          search.set(changeKey, changeValue);
         } else {
-          history.push({ search: search.toString() })
+          search.append(changeKey, changeValue);
+        }
+      });
+
+      // pushing to history
+      if (search && history) {
+        if (replace) {
+          history.replace({ search: search.toString() });
+        } else {
+          history.push({ search: search.toString() });
         }
       }
-      console.log('changing history', search.toString())
     }
   }
-}
+};
 
-module.exports = Base
+module.exports = Base;
