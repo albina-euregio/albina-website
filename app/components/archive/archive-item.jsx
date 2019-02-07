fa612cd1d825f1af32b526c242f5144d8376d969
adammertel <mertel.adam@gmail.com>
2019-02-07 16:43:07 +0100

    archive pdf links

--- app/components/archive/archive-item.jsx
+++ app/components/archive/archive-item.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { inject } from 'mobx-react'
-import { injectIntl, FormattedHTMLMessage } from 'react-intl'
-import stringInject from 'stringinject'
-import {
-  dateToISODateString,
-  dateToDateString
-} from '../../util/date.js'
+import React from "react";
+import { Link } from "react-router-dom";
+import { inject } from "mobx-react";
+import { injectIntl, FormattedHTMLMessage } from "react-intl";
+import stringInject from "stringinject";
+import { dateToISODateString, dateToDateString } from "../../util/date.js";
 
 class ArchiveItem extends React.Component {
   get previewMap() {
     // TODO: fix daytime
     return (
-      window['config'].get('apis.geo') +
+      window["config"].get("apis.geo") +
       dateToISODateString(this.props.date) +
-      '/am_albina_thumbnail.jpg'
-    )
+      "/am_albina_thumbnail.jpg"
+    );
   }
 
   render() {
-    const dateString = dateToISODateString(this.props.date)
+    const dateString = dateToISODateString(this.props.date);
+
+    const baseLink = config.get("links.downloads")["base"];
     return (
       <tr>
         <td>
@@ -29,13 +28,16 @@ class ArchiveItem extends React.Component {
           <ul className="list-inline list-download">
             <li>
               <a
-                href={stringInject(
-                  config.get('links.downloads.pdf'),
-                  { date: dateString, lang: this.props.lang }
-                )}
+                href={
+                  baseLink +
+                  stringInject(config.get("links.downloads.pdf"), {
+                    date: dateString,
+                    lang: this.props.lang
+                  })
+                }
                 target="_blank"
                 title={this.props.intl.formatMessage({
-                  id: 'archive:download-pdf:hover'
+                  id: "archive:download-pdf:hover"
                 })}
                 className="small secondary pure-button tooltip"
               >
@@ -44,12 +46,15 @@ class ArchiveItem extends React.Component {
             </li>
             <li>
               <a
-                href={stringInject(
-                  config.get('links.downloads.xml'),
-                  { date: dateString, lang: this.props.lang }
-                )}
+                href={
+                  baseLink +
+                  stringInject(config.get("links.downloads.xml"), {
+                    date: dateString,
+                    lang: this.props.lang
+                  })
+                }
                 title={this.props.intl.formatMessage({
-                  id: 'archive:download-xml:hover'
+                  id: "archive:download-xml:hover"
                 })}
                 target="_blank"
                 className="small secondary pure-button tooltip"
@@ -61,18 +66,18 @@ class ArchiveItem extends React.Component {
         </td>
         <td>
           <Link
-            to={'/bulletin/' + dateString}
+            to={"/bulletin/" + dateString}
             className="map-preview img tooltip"
             title={this.props.intl.formatMessage({
-              id: 'archive:show-forecast:hover'
+              id: "archive:show-forecast:hover"
             })}
           >
             <img src={this.previewMap} alt="Region" />
           </Link>
         </td>
       </tr>
-    )
+    );
   }
 }
 
-export default inject('locale')(injectIntl(ArchiveItem))
+export default inject("locale")(injectIntl(ArchiveItem));
