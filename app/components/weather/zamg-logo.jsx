0aa3eb13827be953cb3d34d96f97f7abfd080b60
Leonhard Brunauer <leonhard.brunauer@gmx.at>
2019-05-23 13:38:09 +0200

    Align controls horizontally.

--- app/components/weather/zamg-logo.jsx
+++ app/components/weather/zamg-logo.jsx
@@ -4,7 +4,7 @@ import Control from "react-leaflet-control";
 export default class ZamgLogo extends React.Component {
   render() {
     return (
-      <Control position="bottomright">
+      <Control position="bottomright" className="pad-right">
         <a href="https://www.zamg.ac.at" className="pure-button secondary" target="_blank"><div id="zamg-logo"></div></a>
       </Control>
     );
