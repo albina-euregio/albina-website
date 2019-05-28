381cd0fecbb780cefa42b99a714f48ce2cece52f
Leonhard Brunauer <leonhard.brunauer@gmx.at>
2019-05-28 09:32:59 +0200

    Fixed styles of zamg logo.

--- app/components/weather/zamg-control.jsx
+++ app/components/weather/zamg-control.jsx
@@ -4,7 +4,7 @@ import Control from "react-leaflet-control";
 export default class ZamgControl extends React.Component {
   render() {
     return (
-      <Control position="bottomright" className="pad-right">
+      <Control position="bottomright" className="zamg-logo-container">
         <a href="https://www.zamg.ac.at" className="pure-button secondary" target="_blank"><div id="zamg-logo"></div></a>
       </Control>
     );
