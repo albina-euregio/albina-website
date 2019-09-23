ac39fa4fe257046a16766d095d9332148d708f83
Leonhard Kreil-Brunauer <leonhard.brunauer@gmx.at>
2019-09-23 15:53:51 +0200

    Make report list an observer.

--- app/components/bulletin/bulletin-list.jsx
+++ app/components/bulletin/bulletin-list.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { observer } from "mobx-react";
 import BulletinReport from "./bulletin-report";
 
+@observer
 export default class BulletinList extends React.Component {
     constructor(props) {
         super(props);
@@ -13,11 +15,15 @@ export default class BulletinList extends React.Component {
     }
 
     render() {
+        const activeRegion =this.props.store.activeBulletin ? this.props.store.activeBulletin.id : null;
         return (
             <ul className="bulletin-list">
-                {this.bulletins.map((bulletin) => 
-                    <li key={bulletin.id}>
-                        <BulletinReport bulletin={bulletin} store={this.props.store} />
+                {this.bulletins.map((bulletin) =>
+                    <li key={bulletin.id} className={(bulletin.id === activeRegion) ? "selected" : ""}>
+                        { (bulletin.id === activeRegion) 
+                            ? <BulletinReport bulletin={bulletin} date={this.props.store.settings.date} />
+                            : <BulletinReport bulletin={bulletin} date={this.props.store.settings.date} />
+                        }
                     </li>
                 )}
             </ul>
