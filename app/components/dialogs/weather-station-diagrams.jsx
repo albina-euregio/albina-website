d999d7fcc5ce508b4cf968b676284cfba7b1bf10
Thomas Wanner <wanner@transporter.at>
2019-11-22 19:04:14 +0100

    renew images periodically

--- app/components/dialogs/weather-station-diagrams.jsx
+++ app/components/dialogs/weather-station-diagrams.jsx
@@ -7,16 +7,24 @@ class WeatherStationDiagrams extends React.Component {
     super(props);
   }
 
+  get cacheHash() {
+    let currentTS = new Date();
+    currentTS.setMilliseconds(0);
+    currentTS.setSeconds(0);
+    currentTS.setMinutes(Math.round(currentTS.getMinutes() / 5) * 5);
+    return currentTS.valueOf();
+  }
+
 
   render() {
     let stationData = window["modalStateStore"].data.stationData;
-    console.log('stationData', stationData);
+    // console.log('stationData', stationData);
 
     if(!stationData) return (<div></div>);
     return (
       <h2 className="subheader">
           Weather Station Info for {stationData.name}
-          <img src={'https://lawine.tirol.gv.at/data/grafiken/540/standard/dreitage/' + stationData.plot + '.png'} />
+          <img src={'https://lawine.tirol.gv.at/data/grafiken/540/standard/dreitage/' + stationData.plot + '.png?' + this.cacheHash} />
         </h2>
     );
   }
