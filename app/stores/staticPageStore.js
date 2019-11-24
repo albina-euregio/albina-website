a049b5fecb6fc7da3342399fff4eaf58abd36238
Simon Legner <Simon.Legner@gmail.com>
2019-11-24 14:16:18 +0100

    staticPageStore: set shareable

--- app/stores/staticPageStore.js
+++ app/stores/staticPageStore.js
@@ -4,12 +4,13 @@ export default class StaticPageStore {
   loadPage(url) {
     const lang = window["appStore"].language;
     return axios.get(`/content/${url}/${lang}.html`).then(response => {
+      const sharable = true;
       const body = response.data;
       let title = body.match(/^<!--\s*title:\s*(.*)-->/);
       title = title ? title[1] : undefined;
       return {
         data: {
-          attributes: { title, body }
+          attributes: { title, body, sharable }
         }
       };
     });
