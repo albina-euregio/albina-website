47cdc20c2ae8b886b4b54af03da9caced1cbcf9f
Thomas Wanner <wanner@transporter.at>
2019-12-10 13:54:11 +0100

    hide filter

--- app/views/blogOverview.jsx
+++ app/views/blogOverview.jsx
@@ -161,7 +161,7 @@ class BlogOverview extends React.Component {
             value={this.store.regionActive}
             className={this.store.regionActive !== "all" ? classChanged : ""}
           />
-          <TagFilter
+          {/* <TagFilter
             title={this.props.intl.formatMessage({
               id: "blog:filter:avalanche-problem"
             })}
@@ -175,7 +175,7 @@ class BlogOverview extends React.Component {
                 ? "disabled"
                 : ""
             }
-          />
+          /> */}
           <YearFilter
             title={this.props.intl.formatMessage({
               id: "blog:filter:year"
