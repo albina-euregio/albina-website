8550f14e9aec724e107570d5b6a4ac5f04add698
Simon Legner <Simon.Legner@gmail.com>
2019-12-20 14:35:51 +0100

    Obtain tippy.js from npm

--- app/main.jsx
+++ app/main.jsx
@@ -38,9 +38,6 @@ window["jQuery"] = window["$"] = require("jquery");
 require("./bower_components/jquery-selectric/public/jquery.selectric.min.js");
 require("./bower_components/tilt_1.1.19/dest/tilt.jquery.min.js");
 require("./bower_components/magnific-popup_1.1.0/dist/jquery.magnific-popup.min.js");
-window[
-  "tippy"
-] = require("./bower_components/tippyjs_2.2.3/dist/tippy.all.min.js");
 
 // TODO: check content API for maintenance mode before starting the app
 window["appStore"] = new AppStore();
