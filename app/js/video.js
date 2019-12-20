5d1c7d1943f67ad33aadc3f75d8d3397029a5b33
Thomas Wanner <wanner@transporter.at>
2019-12-20 17:03:48 +0100

    hack vidoes to be dispayed

--- app/js/video.js
+++ app/js/video.js
@@ -1,10 +1,19 @@
 import fluidvids from "fluidvids.js";
 
 function video_init() {
-  fluidvids.init({
-    selector: [".fitvids iframe"], // runs querySelectorAll()
-    players: ["www.youtube.com", "player.vimeo.com"] // players to support
-  });
+  setTimeout(() => {
+    console.log("setTimeout");
+    const items = document.getElementsByTagName("iframe");
+
+    for (let item of items) {
+      item.classList.remove("displayNone");
+    }
+
+    fluidvids.init({
+      selector: [".fitvids iframe"], // runs querySelectorAll()
+      players: ["www.youtube.com", "player.vimeo.com"] // players to support
+    });
+  }, 300);
 }
 
 export { video_init };
