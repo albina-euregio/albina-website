4bc9e58dfd2430c6f618218b5c6dbdaf860bd777
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2020-02-24 08:57:05 +0100

    fix(zamg-models): delete unused import

--- src/app/modelling/zamg-models.component.ts
+++ src/app/modelling/zamg-models.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ModellingService, ZamgModelPoint } from "./modelling.service";
 
 @Component({
