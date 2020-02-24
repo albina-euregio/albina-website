91590d306bc67c1b70bff52af7e428903c46d672
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2020-02-24 09:17:36 +0100

    fix(zamg-models): fix autofocus

--- src/app/modelling/zamg-models.component.ts
+++ src/app/modelling/zamg-models.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterViewInit, ViewChild } from "@angular/core";
 import { ModellingService, ZamgModelPoint } from "./modelling.service";
 
 @Component({
   templateUrl: "./zamg-models.component.html"
 })
-export class ZamgModelsComponent implements OnInit {
+export class ZamgModelsComponent implements OnInit, AfterViewInit {
   modelPoints: ZamgModelPoint[];
   selectedModelPoint: ZamgModelPoint;
+
+  @ViewChild("select") select;
 
   constructor(private modellingService: ModellingService) {}
 
@@ -15,5 +17,9 @@ export class ZamgModelsComponent implements OnInit {
       this.modelPoints = zamgModelPoints;
       this.selectedModelPoint = zamgModelPoints[0];
     });
+  }
+
+  ngAfterViewInit() {
+    this.select.nativeElement.focus();
   }
 }
