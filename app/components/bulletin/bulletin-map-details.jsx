6157888ffc05c50c47b1329342c589620750c550
Thomas Wanner <wanner@transporter.at>
2020-09-22 15:26:45 +0200

    enable public alert

--- app/components/bulletin/bulletin-map-details.jsx
+++ app/components/bulletin/bulletin-map-details.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { observer } from "mobx-react";
 import ProblemIconLink from "../icons/problem-icon-link.jsx";
 import BulletinDangerRating from "./bulletin-danger-rating.jsx";
+import { inject } from "mobx-react";
+import { injectIntl } from "react-intl";
 
 /**
  * @typedef {object} Props
@@ -10,31 +12,51 @@ import BulletinDangerRating from "./bulletin-danger-rating.jsx";
  * @extends {React.Component<Props>}
  */
 @observer
-export default class BulletinMapDetails extends React.Component {
+class BulletinMapDetails extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     // TODO: create common component with bulletin-report
+
     const daytime =
       this.props.bulletin.hasDaytimeDependency && this.props.ampm == "pm"
         ? "afternoon"
         : "forenoon";
     const bulletin = this.props.bulletin[daytime];
     const problems = bulletin.avalancheProblems || [];
+    console.log("BulletinMapDetails", bulletin);
 
     return (
-      <ul className="list-plain">
-        <li className="bulletin-report-picto tooltip">
-          <BulletinDangerRating bulletin={bulletin} />
-        </li>{" "}
-        {problems.map(problem => (
-          <li key={problem.type}>
-            <ProblemIconLink problem={problem} />
-          </li>
-        ))}
-      </ul>
+      <>
+        <ul className="list-plain">
+          <li className="bulletin-report-picto tooltip">
+            <BulletinDangerRating bulletin={bulletin} />
+          </li>{" "}
+          {problems.map(problem => (
+            <li key={problem.type}>
+              <ProblemIconLink problem={problem} />
+            </li>
+          ))}
+        </ul>
+
+        {bulletin.highlights && (
+          <p className="bulletin-report-public-alert">
+            <span
+              className="warning label tooltip"
+              title={this.props.intl.formatMessage({
+                id: "bulletin:map:details:warning:title"
+              })}
+            >
+              !
+            </span>
+            {bulletin.highlights}
+          </p>
+        )}
+      </>
     );
   }
 }
+
+export default inject("locale")(injectIntl(BulletinMapDetails));
