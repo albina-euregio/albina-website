31193bc3016e293c3a56109a06d0defae03b030a
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2020-11-16 08:08:02 +0100

    fix: allow only de, it and en as languages for blog posts

--- app/components/dialogs/subscribe-email-dialog.jsx
+++ app/components/dialogs/subscribe-email-dialog.jsx
@@ -147,7 +147,7 @@ class SubscribeEmailDialog extends React.Component {
               <span className="normal" />
             </label>
             <ul className="list-inline list-subscribe-language">
-              {window["appStore"].emailLanguages.map(l => (
+              {window["appStore"].mainLanguages.map(l => (
                 <li key={l}>
                   <label
                     className="pure-checkbox"
