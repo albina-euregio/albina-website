d3e802a830b1e47b08f8cafd25159a1ecd5218d5
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2020-12-02 08:34:58 +0100

    chore(textcat): change textcat url to textcat-ng

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiBaseUrl: "https://admin.avalanche.report/albina/api/",
   wsBaseUrl: "wss://socket.avalanche.report/albina/",
-  textcatUrl: "https://admin.avalanche.report/textcat/c_pm.html",
+  textcatUrl: "https://admin.avalanche.report/textcat-ng/",
   headerBgColor: "#ffffff",
   showChat: true
 };
