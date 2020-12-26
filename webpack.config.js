ab54fc28802f4d12ed6aaa6fcd1ba599f704cc81
Simon Legner <Simon.Legner@gmail.com>
2020-12-26 19:56:22 +0100

    chore(webpack): increase maxAssetSize

--- webpack.config.js
+++ webpack.config.js
@@ -107,8 +107,8 @@ module.exports = (env, argv) => {
     stats: "errors-only",
     performance: {
       hints: production ? "error" : false,
-      maxEntrypointSize: 2.1 * mebibyte,
-      maxAssetSize: 1.9 * mebibyte
+      maxEntrypointSize: 2.2 * mebibyte,
+      maxAssetSize: 2.0 * mebibyte
     },
     plugins: [
       new HtmlWebPackPlugin({
