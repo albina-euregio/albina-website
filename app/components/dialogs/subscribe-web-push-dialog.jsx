dbfcf3a827a2000bc59606df35a347aafa65309e
Simon Legner <Simon.Legner@gmail.com>
2021-01-19 00:01:36 +0100

    feat(subscribe-web-push-dialog): language + region selection

--- app/components/dialogs/subscribe-web-push-dialog.jsx
+++ app/components/dialogs/subscribe-web-push-dialog.jsx
@@ -1,10 +1,11 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { useIntl } from "react-intl";
+import { FormattedHTMLMessage, useIntl } from "react-intl";
+import ProvinceFilter from "../filters/province-filter";
 
 /**
  * @param {PushSubscription} subscription
  */
-function updatePushSubscription(subscription, url) {
+function updatePushSubscription(subscription, url, language = "", region = "") {
   const { endpoint, keys } = subscription.toJSON();
   return fetch(url, {
     method: "POST",
@@ -15,8 +16,8 @@ function updatePushSubscription(subscription, url) {
       endpoint: endpoint,
       auth: keys.auth,
       p256dh: keys.p256dh,
-      language: window["appStore"].language,
-      region: ""
+      language,
+      region
     })
   });
 }
@@ -52,6 +53,9 @@ export function isWebPushSupported() {
  * @see https://developers.google.com/web/fundamentals/push-notifications
  */
 export default function SubscribeWebPushDialog() {
+  const [language, setLanguage] = useState(window["appStore"].language);
+  const [region, setRegion] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
   const [isSubscribed, setIsSubscribed] = useState(false);
   const intl = useIntl();
 
@@ -60,45 +64,163 @@ export default function SubscribeWebPushDialog() {
   }
 
   const handleEnable = useCallback(async () => {
-    const permission = await Notification.requestPermission();
-    if (permission !== "granted") return;
-    const registration = await navigator.serviceWorker.getRegistration();
-    const subscription = await registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(config.apiKeys.vapidPublicKey)
-    });
-    if (!subscription) return;
-    await updatePushSubscription(subscription, `${config.apis.push}/subscribe`);
-    setIsSubscribed(true);
-  }, []);
+    setErrorMessage(undefined);
+    try {
+      const permission = await Notification.requestPermission();
+      if (permission !== "granted") {
+        throw intl.formatMessage({
+          id: "dialog:subscribe-web-push:error:no-permission"
+        });
+      }
+      const registration = await navigator.serviceWorker.getRegistration();
+      const subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          config.apiKeys.vapidPublicKey
+        )
+      });
+      if (!subscription) {
+        throw intl.formatMessage({
+          id: "dialog:subscribe-web-push:error:no-subscription"
+        });
+      }
+      await updatePushSubscription(
+        subscription,
+        `${config.apis.push}/subscribe`,
+        language,
+        region
+      );
+      setIsSubscribed(true);
+    } catch (reason) {
+      setErrorMessage(String(reason));
+      throw reason;
+    }
+  }, [setErrorMessage, language, region, intl]);
 
   const handleDisable = useCallback(async () => {
-    const registration = await navigator.serviceWorker.getRegistration();
-    const subscription = await registration.pushManager.getSubscription();
-    if (!subscription) return;
-    await subscription.unsubscribe();
-    await updatePushSubscription(
-      subscription,
-      `${config.apis.push}/unsubscribe`
-    );
-    setIsSubscribed(false);
-  }, []);
+    setErrorMessage(undefined);
+    try {
+      const registration = await navigator.serviceWorker.getRegistration();
+      const subscription = await registration.pushManager.getSubscription();
+      if (!subscription) return;
+      await subscription.unsubscribe();
+      await updatePushSubscription(
+        subscription,
+        `${config.apis.push}/unsubscribe`
+      );
+      setIsSubscribed(false);
+    } catch (reason) {
+      setErrorMessage(String(reason));
+      throw reason;
+    }
+  }, [setErrorMessage, setIsSubscribed]);
 
   useEffect(() => {
     (async () => {
-      const registration = await navigator.serviceWorker.getRegistration();
-      const subscription = await registration.pushManager.getSubscription();
-      setIsSubscribed(Boolean(subscription));
+      try {
+        const registration = await navigator.serviceWorker.getRegistration();
+        const subscription = await registration.pushManager.getSubscription();
+        setIsSubscribed(Boolean(subscription));
+      } catch (reason) {
+        setErrorMessage(String(reason));
+        throw reason;
+      }
     })();
-  }, [setIsSubscribed]);
+  }, [setErrorMessage, setIsSubscribed]);
 
-  return isSubscribed ? (
-    <button className="pure-button" onClick={handleDisable}>
-      {intl.formatMessage({ id: "dialog:subscribe-web-push:disable" })}
-    </button>
-  ) : (
-    <button className="pure-button" onClick={handleEnable}>
-      {intl.formatMessage({ id: "dialog:subscribe-web-push:enable" })}
-    </button>
+  return (
+    <div className="modal-subscribe-telegram">
+      <div className="modal-header">
+        <h2>
+          <FormattedHTMLMessage id="dialog:subscribe-telegram:subheader" />
+        </h2>
+      </div>
+
+      {isSubscribed && (
+        <button
+          type="submit"
+          className="pure-button"
+          onClick={() => handleDisable()}
+        >
+          {intl.formatMessage({
+            id: "dialog:subscribe-web-push:disable"
+          })}
+        </button>
+      )}
+
+      {!isSubscribed && (
+        <form
+          className="pure-form pure-form-stacked"
+          onSubmit={event => {
+            event.preventDefault();
+            event.stopPropagation();
+            handleEnable();
+          }}
+        >
+          <label htmlFor="province">
+            <FormattedHTMLMessage id="dialog:subscribe-telegram:region" />
+          </label>
+          <ul className="list-inline list-buttongroup">
+            <li>
+              <ProvinceFilter
+                name="province"
+                className={region && "selectric-changed"}
+                handleChange={r => setRegion(r)}
+                value={region}
+                none={intl.formatMessage({
+                  id: "blog:filter:province:nothing-selected"
+                })}
+              />
+            </li>
+          </ul>
+          <label htmlFor="language">
+            <FormattedHTMLMessage id="dialog:subscribe-telegram:language" />
+            <span className="normal" />
+          </label>
+          <ul className="list-inline list-subscribe-language">
+            {window["appStore"].mainLanguages.map(l => (
+              <li key={l}>
+                <label
+                  className="pure-checkbox"
+                  htmlFor={"subscribe-language-" + l}
+                >
+                  <input
+                    id={"subscribe-language-" + l}
+                    name="language"
+                    onChange={() => setLanguage(l)}
+                    value={l}
+                    type="radio"
+                    checked={language === l ? "checked" : ""}
+                  />
+                  &nbsp;
+                  <span className="normal">{l.toUpperCase()}</span>
+                </label>
+              </li>
+            ))}
+          </ul>
+          <button
+            type="submit"
+            className="pure-button"
+            disabled={region && language ? "" : "disabled"}
+          >
+            {intl.formatMessage({
+              id: "dialog:subscribe-web-push:enable"
+            })}
+          </button>
+        </form>
+      )}
+      {errorMessage && (
+        <div className="field-2 panel">
+          <p className="status-message">
+            <strong className="error">
+              {intl.formatMessage({
+                id: "dialog:subscribe-web-push:error"
+              })}
+            </strong>
+            &nbsp;{errorMessage}
+          </p>
+        </div>
+      )}
+    </div>
   );
 }
