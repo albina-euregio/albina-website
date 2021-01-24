fb0784a36989314d484e01af1532a1b3ca644510
Simon Legner <Simon.Legner@gmail.com>
2021-01-24 20:49:59 +0100

    refact(bulletinStore): do not call /status

--- app/components/bulletin/bulletin-status-line.jsx
+++ app/components/bulletin/bulletin-status-line.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { observer } from "mobx-react";
-import { computed } from "mobx";
 import { injectIntl } from "react-intl";
 import {
   dateToDateString,
@@ -19,24 +18,20 @@ class BulletinStatusLine extends React.Component {
     super(props);
   }
 
-  @computed get message() {
-    return window["archiveStore"].getStatusMessage(
-      this.props.store.settings.date
-    );
-  }
-
-  @computed get statusText() {
+  render() {
     const collection = this.props.store.activeBulletinCollection;
+    let statusText = "";
+    let isRepublished = false;
 
     if (this.props.status == "pending") {
-      return (
+      statusText =
         this.props.intl.formatMessage({ id: "bulletin:header:loading" }) +
-        "\u2026"
-      );
+        "\u2026";
     }
 
     if (this.props.status == "ok") {
       const pubDate = getLocalDate(collection.publicationDate);
+      isRepublished = pubDate.getHours() !== 17 || pubDate.getMinutes() !== 0;
 
       // There must be a status entry for each downloaded bulletin. Query its
       // original status message.
@@ -45,33 +40,29 @@ class BulletinStatusLine extends React.Component {
         time: dateToTimeString(pubDate)
       };
 
-      if (this.message === "republished") {
-        return this.props.intl.formatMessage(
+      if (isRepublished) {
+        statusText = this.props.intl.formatMessage(
           { id: "bulletin:header:updated-at" },
           params
         );
       } else {
-        return this.props.intl.formatMessage(
+        statusText = this.props.intl.formatMessage(
           { id: "bulletin:header:published-at" },
           params
         );
       }
     }
 
-    return "";
-  }
-
-  render() {
     return (
       <p
         className={
           "marginal " +
-          (this.message === "republished"
+          (isRepublished
             ? "bulletin-datetime-publishing"
             : "bulletin-datetime-validity")
         }
       >
-        {this.statusText}
+        {statusText}
       </p>
     );
   }
