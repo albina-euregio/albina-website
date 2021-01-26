fa6234e8d032c1a0766540e5d48929d6414e957b
Simon Legner <Simon.Legner@gmail.com>
2021-01-26 08:30:30 +0100

    refact(natlefs.component): use ngFor

--- src/app/observations/natlefs.component.ts
+++ src/app/observations/natlefs.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from "@angular/core";
-import { TranslateService } from "@ngx-translate/core";
 import { Natlefs } from "app/models/natlefs.model";
 
 @Component({
@@ -7,43 +6,5 @@ import { Natlefs } from "app/models/natlefs.model";
   templateUrl: "natlefs.component.html"
 })
 export class NatlefsComponent {
-
   @Input() natlefs: Natlefs;
-
-  constructor(private translateService: TranslateService) {}
-
-  hasCoordinates() {
-    return (
-      this.natlefs?.location?.geo?.latitude &&
-      this.natlefs?.location?.geo?.longitude
-    );
-  }
-
-  getSnowConditionsString() {
-    return this.natlefs.snowConditions
-      .map((c) => this.translateService.instant("snowConditions." + c))
-      .join(", ");
-  }
-
-  getRodeString() {
-    return this.natlefs.rode
-      .map((r) => this.translateService.instant("terrainFeature." + r))
-      .join(", ");
-  }
-
-  getAvoidedString() {
-    return this.natlefs.avoided
-      .map((t) => this.translateService.instant("terrainFeature." + t))
-      .join(", ");
-  }
-
-  getAvalancheProblemsString() {
-    return this.natlefs.avalancheProblems
-      .map((p) => this.translateService.instant("avalancheProblemNatlefs." + p))
-      .join(", ");
-  }
-
-  getAccuracy() {
-    return Math.round(this.natlefs.location.accuracy);
-  }
 }
