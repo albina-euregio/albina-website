b34a1e84edd55621889d4af8ac4a98953c7eb484
Simon Legner <Simon.Legner@gmail.com>
2021-01-26 08:43:53 +0100

    refact(natlefs.model): enums

--- src/app/models/natlefs.model.ts
+++ src/app/models/natlefs.model.ts
@@ -1,29 +1,70 @@
 export interface Natlefs {
-  snowConditions?: string[];
-  driftingSnow: string;
+  snowConditions?: SnowCondition[];
+  driftingSnow: DriftingSnow;
   author: Author;
-  penetrationDepth?: string;
-  rode?: string[];
-  alarmSigns?: string;
-  tracks: string;
-  avoided?: string[];
-  datetime: Datetime;
-  avalancheProblems?: string[];
-  avalanches: string;
-  newSnow: string;
+  penetrationDepth?: PenetrationDepth;
+  rode?: TerrainFeature[];
+  alarmSigns?: AlarmSigns;
+  tracks: Avalanches;
+  avoided?: TerrainFeature[];
+  datetime: DateTime;
+  avalancheProblems?: AvalancheProblem[];
+  avalanches: Avalanches;
+  newSnow: NewSnow;
   location: Location;
-  surfaceSnowWetness?: string;
-  ridingQuality?: string;
+  surfaceSnowWetness?: SurfaceSnowWetness;
+  ridingQuality?: RidingQuality;
   comment?: string;
+}
+
+export enum AlarmSigns {
+  Frequent = "frequent",
+  None = "none",
+  Occasional = "occasional",
 }
 
 export interface Author {
   name: string;
 }
 
-export interface Datetime {
+export enum AvalancheProblem {
+  FavourableSituation = "favourable_situation",
+  GlidingSnow = "gliding_snow",
+  NewSnow = "new_snow",
+  WeakPersistentLayer = "weak_persistent_layer",
+  WindDriftedSnow = "wind_drifted_snow",
+}
+
+export enum Avalanches {
+  Many = "many",
+  None = "none",
+  Some = "some",
+}
+
+export enum TerrainFeature {
+  ConvexSlopes = "convexSlopes",
+  DenseTrees = "denseTrees",
+  ModeratelySteepSlopes = "moderatelySteepSlopes",
+  OpenTrees = "openTrees",
+  ShadySlopes = "shadySlopes",
+  SteepSlopes = "steepSlopes",
+  SunnySlopes = "sunnySlopes",
+  VerySteepSlopes = "verySteepSlopes",
+}
+
+export interface DateTime {
   date: Date;
-  quality: string;
+  quality: Quality;
+}
+
+export enum Quality {
+  Measured = "measured",
+}
+
+export enum DriftingSnow {
+  None = "none",
+  Occasional = "occasional",
+  Widespread = "widespread",
 }
 
 export interface Location {
@@ -38,4 +79,39 @@ export interface Location {
 export interface Geo {
   latitude: number;
   longitude: number;
+}
+
+export enum NewSnow {
+  High = "high",
+  Low = "low",
+  Medium = "medium",
+  None = "none",
+}
+
+export enum PenetrationDepth {
+  Little = "little",
+  Medium = "medium",
+  Much = "much",
+  Spotty = "spotty",
+}
+
+export enum RidingQuality {
+  Amazing = "amazing",
+  Good = "good",
+  Indifferent = "indifferent",
+  Ok = "ok",
+}
+
+export enum SnowCondition {
+  Crusty = "crusty",
+  Hard = "hard",
+  Heavy = "heavy",
+  Powder = "powder",
+  Wet = "wet",
+  WindAffected = "windAffected",
+}
+
+export enum SurfaceSnowWetness {
+  Dry = "dry",
+  Sticky = "sticky",
 }
