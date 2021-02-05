1dd36feaebb121270549d64913d46356ce95bf89
Simon Legner <Simon.Legner@gmail.com>
2021-02-05 23:13:09 +0100

    feat(observation-editor): geocode location
    Uses <p-autocomplete> to geolocate the given location in order to
    determine latitude and longitude.

--- src/app/observations/observations.service.ts
+++ src/app/observations/observations.service.ts
@@ -9,6 +9,7 @@ import { convertLoLaToGeneric, LoLaSafety, LoLaSafetyApi } from "app/models/lola
 import { Lawis, Profile, Incident, IncidentDetails, parseLawisDate, toLawisIncidentTable } from "app/models/lawis.model";
 import { GenericObservation, Source, toAspect } from "app/models/generic-observation.model";
 import { TranslateService } from "@ngx-translate/core";
+import { FeatureCollection, Point } from "geojson";
 
 @Injectable()
 export class ObservationsService {
@@ -183,6 +184,21 @@ export class ObservationsService {
   private inDateRange(date: Date): boolean {
     return this.startDate <= date && date <= this.endDate;
   }
+
+  async searchLocation(query: string, limit = 8): Promise<FeatureCollection<Point, GeocodingProperties>> {
+    // https://nominatim.org/release-docs/develop/api/Search/
+    const { osmNominatimApi, osmNominatimCountries } = this.constantsService;
+    const params: Record<string, string> = {
+      "accept-language": this.translateService.currentLang,
+      countrycodes: osmNominatimCountries,
+      format: "geojson",
+      limit: String(limit),
+      q: query
+    };
+    return this.http
+      .get<FeatureCollection<Point, GeocodingProperties>>(osmNominatimApi, { params })
+      .toPromise();
+  }
 }
 
 function getISOString(date: Date) {
@@ -203,4 +219,15 @@ function getISOString(date: Date) {
   function pad(number: number): string {
     return number < 10 ? `0${number}` : `${number}`;
   }
+}
+
+export interface GeocodingProperties {
+  place_id: number;
+  osm_type: string;
+  osm_id: number;
+  display_name: string;
+  place_rank: number;
+  category: string;
+  type: string;
+  importance: number;
 }
