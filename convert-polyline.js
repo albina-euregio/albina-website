e18c5ad0d57a16dfa4b62e546d26ce484f0b237e
Simon Legner <Simon.Legner@gmail.com>
2021-04-08 08:47:54 +0200

    refact: use micro-regions for micro_regions

--- convert-polyline.js
+++ convert-polyline.js
@@ -3,17 +3,34 @@ const fs = require("fs");
 const { encodeFeatureCollection } = require("./app/util/polyline");
 
 /**
- * @param {string} filename
+ * @param {string[]} files
+ * @param {string} output
  */
-function encodeFile(filename) {
-  const geojson = JSON.parse(fs.readFileSync(filename));
+function encodeFiles(files, output) {
+  /**
+   * @type {[GeoJSON.FeatureCollection]}
+   */
+  const geojsons = files.map(file => JSON.parse(fs.readFileSync(file)));
+  /**
+   * @type {GeoJSON.FeatureCollection}
+   */
+  const geojson = {
+    type: "FeatureCollection",
+    features: []
+  };
+  geojson.features = geojson.features
+    .concat(...geojsons.map(g => g.features))
+    .map(f => ({ id: f.properties?.id, ...f }))
+    .sort((f1, f2) => f1.properties.id.localeCompare(f2.properties.id));
   const polyline = encodeFeatureCollection(geojson);
-  fs.writeFileSync(
-    filename.replace(/geojson.json$/, "polyline.json"),
-    JSON.stringify(polyline, undefined, 2)
-  );
+  fs.writeFileSync(output, JSON.stringify(polyline, undefined, 2));
 }
 
-encodeFile("app/stores/micro_regions.geojson.json");
-encodeFile("app/stores/neighbor_regions.geojson.json");
-encodeFile("app/stores/neighbor_micro_regions.geojson.json");
+encodeFiles(
+  [
+    "eaws-regions/public/micro-regions/AT-07_micro-regions.geojson.json",
+    "eaws-regions/public/micro-regions/IT-32-BZ_micro-regions.geojson.json",
+    "eaws-regions/public/micro-regions/IT-32-TN_micro-regions.geojson.json"
+  ],
+  "app/stores/micro_regions.polyline.json"
+);
