38fbc1ebc4f085eae7c747e408e1110912aef298
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2021-04-26 15:05:40 +0200

    refactor: formatting

--- app/components/bulletin/bulletin-awmap-static.jsx
+++ app/components/bulletin/bulletin-awmap-static.jsx
@@ -29,16 +29,15 @@ class BulletinAWMapStatic extends React.Component {
       publicationDirectory +
       this.props.region + // possibly contains _PM
       imgFormat;
-    const regions = window["bulletinStore"].bulletins[this.props.date].daytimeBulletins.find(element => element.id == this.props.region.split("_")[0]).forenoon.regions.map(function(elem) {
-      return elem.name;
-  }).join(", ");
-    return (
-      <img
-        src={url}
-        alt={regions}
-        onError={this.props.onError}
-      />
-    );
+    const regions = window["bulletinStore"].bulletins[
+      this.props.date
+    ].daytimeBulletins
+      .find(element => element.id == this.props.region.split("_")[0])
+      .forenoon.regions.map(function(elem) {
+        return elem.name;
+      })
+      .join(", ");
+    return <img src={url} alt={regions} onError={this.props.onError} />;
   }
 }
 
