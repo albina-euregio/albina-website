de70bd70ff216816c7740d1ce8fb8ee87ce729a1
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2021-04-27 12:42:45 +0200

    chore(social-media-service): delete obsolete methods

--- src/app/providers/configuration-service/configuration.service.ts
+++ src/app/providers/configuration-service/configuration.service.ts
@@ -55,37 +55,4 @@ export class ConfigurationService {
 
     return this.http.post(url, body, options);
   }
-
-  public loadSocialMediaConfiguration(regionId: String): Observable<Response> {
-    const url = this.constantsService.getServerUrl() + "configuration/region?regionId=" + regionId;
-    const options = { headers: this.authenticationService.newAuthHeader() };
-
-    return this.http.get<Response>(url, options);
-  }
-
-  public saveSocialMediaConfiguration(regionConfiguration) {
-    const url = this.constantsService.getServerUrl() + "configuration/region";
-    const body = JSON.stringify(regionConfiguration);
-    const options = { headers: this.authenticationService.newAuthHeader() };
-    return this.http.post<Response>(url, body, options);
-  }
-
-  public loadSocialMediaChannels() {
-    const url = this.constantsService.getServerUrl() + "configuration/channels";
-    const options = { headers: this.authenticationService.newAuthHeader() };
-
-    return this.http.get<Response>(url, options);
-  }
-
-  public loadRecipientList(regionId: String) {
-    const url = this.constantsService.getServerUrl() + "social-media/rapidmail/recipient-list/" + regionId;
-    const authHeader = "Bearer " + this.authenticationService.getAccessToken();
-    const headers = new HttpHeaders({
-      "Content-Type": "application/json",
-      "Accept": "application/hal+json",
-      "Authorization": authHeader
-    });
-    const options = { headers: headers };
-    return this.http.get<Response>(url, options);
-  }
 }
