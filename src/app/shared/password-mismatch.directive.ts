02a757a9dc21e01bfb8c7f79b9f03cd53b9c257f
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2021-04-30 10:13:53 +0200

    fix(create-user): lint

--- src/app/shared/password-mismatch.directive.ts
+++ src/app/shared/password-mismatch.directive.ts
@@ -1,15 +1,15 @@
 import { Directive } from "@angular/core";
-import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, ValidationErrors } from '@angular/forms';
+import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, ValidationErrors } from "@angular/forms";
 
 /** Passwords must match */
 export const passwordMismatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-  const password = control.get('password');
-  const password2 = control.get('password2');
+  const password = control.get("password");
+  const password2 = control.get("password2");
   return password && password2 && password.value === password2.value ? null : { passwordMismatch: true };
 };
 
 @Directive({
-  selector: '[appPasswordMismatch]',
+  selector: "[appPasswordMismatch]",
   providers: [{
     provide: NG_VALIDATORS,
     useExisting: PasswordMismatchValidatorDirective,
