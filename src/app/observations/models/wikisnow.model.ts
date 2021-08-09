ec999d38e556aac01d76d58545dc4c6f167010ae
Simon Legner <Simon.Legner@gmail.com>
2021-08-09 09:23:03 +0200

    feat(observations): include WikisnowECT

---
+++ src/app/observations/models/wikisnow.model.ts
@@ -0,0 +1,27 @@
+export interface WikisnowECT {
+  id: string;
+  images: string;
+  images_url: string;
+  UserName: string;
+  location: string,
+  createDate: string;
+  latlong: string;
+  Sealevel: string;
+  inclination: string;
+  exposition: string;
+  totalSnowDepth: string;
+  ECT_result: string;
+  kind_typ: string;
+  hits: string;
+  groundSnowpack: string;
+  propagation: string;
+  surface: string;
+  weak_layer: string;
+  weak_layer_thickness: string;
+  snowpack: string;
+  description: string;
+  video: null;
+  UserID: null;
+  lat?: number;
+  long?: number;
+}
