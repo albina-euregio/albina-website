2509cbbb2da3c8e0b373f213aa457ce03b7f9ff2
Simon Legner <Simon.Legner@gmail.com>
2021-09-01 16:24:33 +0200

    chore(cluster): simplify map clustering

--- app/components/leaflet/cluster.jsx
+++ app/components/leaflet/cluster.jsx
@@ -1,11 +1,9 @@
-import React from "react"; // eslint-disable-line no-unused-vars
 import PropTypes from "prop-types";
 import { MapLayer } from "react-leaflet";
 import L from "leaflet";
 
 require("leaflet.markercluster");
 require("leaflet.markercluster/dist/MarkerCluster.css");
-require("../../util/placementstrategies");
 
 class Cluster extends MapLayer {
   constructor(props) {
@@ -40,22 +38,7 @@ class Cluster extends MapLayer {
   createLeafletElement() {
     const markerclusters = new L.markerClusterGroup({
       maxClusterRadius: 40,
-      spiderfyDistanceSurplus: 50,
-      spiderfyDistanceMultiplier: 2,
-      elementsPlacementStrategy: "clock",
-      helpingCircles: true,
-      elementsMultiplier: 1.4,
-      firstCircleElements: 8,
       showCoverageOnHover: false,
-      spiderLegPolylineOptions: { weight: 0 },
-      clockHelpingCircleOptions: {
-        weight: 2,
-        opacity: 0.8,
-        fillOpacity: 0,
-        color: "rgb(50, 50, 50)",
-        fill: "black",
-        dashArray: "5 5"
-      },
       iconCreateFunction: this.createClusterIcon.bind(this)
     });
 
