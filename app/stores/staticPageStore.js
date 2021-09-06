9cd118102101d98825f3e5290d14de690b613be4
Simon Legner <Simon.Legner@gmail.com>
2021-09-06 13:12:49 +0200

    refact(StaticPageStore): async function

--- app/stores/staticPageStore.js
+++ app/stores/staticPageStore.js
@@ -1,20 +1,19 @@
 import { fetchText } from "../util/fetch";
 
 export default class StaticPageStore {
-  loadPage(url) {
+  async loadPage(url) {
     const lang = window["appStore"].language;
     url = `${APP_ASSET_PATH}content/${url}/${lang}.html`;
-    return fetchText(url).then(text => {
-      const sharable = true;
-      // extract title from first <h1>...</h1>
-      const titlePattern = /<h1>\s*(.*?)\s*<\/h1>/;
-      const title = text.match(titlePattern)?.[1];
-      const body = text.replace(titlePattern, "");
-      return {
-        data: {
-          attributes: { title, body, sharable }
-        }
-      };
-    });
+    const text = await fetchText(url);
+    const sharable = true;
+    // extract title from first <h1>...</h1>
+    const titlePattern = /<h1>\s*(.*?)\s*<\/h1>/;
+    const title = text.match(titlePattern)?.[1];
+    const body = text.replace(titlePattern, "");
+    return {
+      data: {
+        attributes: { title, body, sharable }
+      }
+    };
   }
 }
