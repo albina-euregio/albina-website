45f3e32b492f628940b40029af2660946f2ccade
Simon Legner <Simon.Legner@gmail.com>
2021-11-08 13:23:23 +0100

    refact(modalStateStore): mobx.makeAutoObservable

--- app/stores/modalStateStore.js
+++ app/stores/modalStateStore.js
@@ -1,33 +1,29 @@
-import { observable, action, makeObservable } from "mobx";
+import { makeAutoObservable } from "mobx";
 
 export default class ModalStateStore {
   constructor() {
-    this._isOpen = observable.box(false);
-    this._data = observable.box(false);
-    makeObservable(this, {
-      open: action,
-      close: action,
-      setData: action
-    });
+    this._isOpen = false;
+    this._data = false;
+    makeAutoObservable(this);
   }
 
   open() {
-    this._isOpen.set(true);
+    this._isOpen = true;
   }
 
   close() {
-    this._isOpen.set(false);
+    this._isOpen = false;
   }
 
   setData(data) {
-    this._data.set(data);
+    this._data = data;
   }
 
   get isOpen() {
-    return this._isOpen.get();
+    return this._isOpen;
   }
 
   get data() {
-    return this._data.get();
+    return this._data;
   }
 }
