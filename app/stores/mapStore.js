10c555181b4936850be563936340bb001333023d
Simon Legner <Simon.Legner@gmail.com>
2021-11-08 13:29:39 +0100

    refact(mapStore): mobx.makeAutoObservable

--- app/stores/mapStore.js
+++ app/stores/mapStore.js
@@ -1,14 +1,10 @@
-import { observable, action, makeObservable } from "mobx";
+import { makeAutoObservable } from "mobx";
 
 export default class MapStore {
   constructor() {
     this.mapCenter = { lat: 47, lon: 12 };
-    this._mapZoom = observable.box(9);
-    makeObservable(this, {
-      setMapViewport: action,
-      zoomIn: action,
-      zoomOut: action
-    });
+    this._mapZoom = 9;
+    makeAutoObservable(this);
   }
 
   setMapViewport(mapState) {
@@ -21,17 +17,17 @@ export default class MapStore {
       this.mapCenter.lat = mapState.center.lat;
       this.mapCenter.lng = mapState.center.lng;
     }
-    this._mapZoom.set(mapState.zoom);
+    this._mapZoom = mapState.zoom;
   }
 
   /**
    * Increase or decrease the zoom value of the bulletin map.
    */
   zoomIn() {
-    this._mapZoom.set(this._mapZoom.get() + 1);
+    this._mapZoom = this._mapZoom.get() + 1;
   }
   zoomOut() {
-    this._mapZoom.set(this._mapZoom.get() - 1);
+    this._mapZoom = this._mapZoom.get() - 1;
   }
 
   /**
@@ -42,6 +38,6 @@ export default class MapStore {
   // }
 
   get mapZoom() {
-    return this._mapZoom.get();
+    return this._mapZoom;
   }
 }
