c4b10be053027eab91b5e0128798a322ea664a89
Simon Legner <Simon.Legner@gmail.com>
2021-11-23 13:50:04 +0100

    feat: Val d'Aran

---
+++ src/environments/environment.aran.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: "https://api.lauegi.report/api/",
+  wsBaseUrl: "wss://socket.avalanche.report/albina/",
+  textcatUrl: "http://conselharan2.cyberneticos.net:8080/textcat-ng/build/",
+  headerBgColor: "#ffffff",
+  showChat: false,
+};
