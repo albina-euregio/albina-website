9877f65e700ee36f78c306bdc4c90bd89f5c620d
Simon Legner <Simon.Legner@gmail.com>
2021-12-02 23:11:06 +0100

    fix(sentry)

--- app/main.jsx
+++ app/main.jsx
@@ -9,6 +9,8 @@ import { isWebPushSupported } from "./components/dialogs/subscribe-web-push-dial
 import jQuery from "jquery";
 window["jQuery"] = window["$"] = jQuery;
 
+import "./sentry";
+
 // TODO: check content API for maintenance mode before starting the app
 window["appStore"] = new AppStore();
 window["staticPageStore"] = new StaticPageStore();
