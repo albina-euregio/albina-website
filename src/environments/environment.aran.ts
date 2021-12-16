34c567c91dde6458afa116dee072a1fc7d769c55
Simon Legner <Simon.Legner@gmail.com>
2021-12-16 22:16:22 +0100

    chore(aran): update textcatUrl

--- src/environments/environment.aran.ts
+++ src/environments/environment.aran.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiBaseUrl: "https://api.lauegi.report/api/",
   wsBaseUrl: "wss://socket.avalanche.report/albina/",
-  textcatUrl: "http://conselharan2.cyberneticos.net:8080/textcat-ng/build/",
+  textcatUrl: "https://admin.lauegi.report/textcat-ng/",
   headerBgColor: "#ffffff",
   showChat: false,
 };
