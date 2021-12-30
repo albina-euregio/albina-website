0701b042b15e3818e425cb77bbe97826c24b3423
Rainer Fabrizi <fabrizi@transporter.at>
2021-12-30 19:07:19 +0100

    Added styles for react tippy #416

--- app/js/tooltip.js
+++ app/js/tooltip.js
@@ -1,4 +1,5 @@
 import tippy, { animateFill } from "tippy.js";
+import "tippy.js/animations/shift-away-subtle.css";
 
 function tooltip_init() {
   //delete tooltips;
@@ -10,10 +11,10 @@ function tooltip_init() {
       console.log("tooltip_init Item", $(this).get(0));
       if ($(this).hasClass("html")) {
         tooltip_delay = 0;
-        tooltip_theme = "custom-html";
+        tooltip_theme = "albina-html"; //"custom-html";
       } else {
         tooltip_delay = window["scroll_duration"] / 4;
-        tooltip_theme = "custom";
+        tooltip_theme = "albina"; //"custom";
       }
       tippy($(this).get(0), {
         plugins: [animateFill],
@@ -23,12 +24,12 @@ function tooltip_init() {
           window["scroll_duration"] / 4
         ],
         delay: [tooltip_delay, tooltip_delay / 2],
-        //,arrow: true
+        arrow: false,
         maxWidth: "30em",
         ////updateDuration: window["scroll_duration"] / 4,
         moveTransition: "transform " + tooltip_delay + "s ease-out",
         theme: tooltip_theme,
-        animation: "shift-away",
+        animation: "shift-away-subtle",
         animateFill: false,
         inertia: true,
         placement: "bottom",
@@ -44,10 +45,10 @@ function tooltip_init() {
             {
               name: "flip",
               // flip: false
-              enabled: false,
+              enabled: true,
               options: {
                 // flipBehavior: ['bottom', 'right', 'top']
-                fallbackPlacements: ["right", "top"]
+                fallbackPlacements: ["top"]
               }
             }
           ]
