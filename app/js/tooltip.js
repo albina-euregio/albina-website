6f9c38798d146ad1782c7758ffa72af6bf2b19a9
TWanner <wanner@transporter.at>
2021-12-30 13:04:24 +0100

    adjust tippy config after version update of tippy from 2 -> 6

--- app/js/tooltip.js
+++ app/js/tooltip.js
@@ -1,12 +1,13 @@
-import tippy from "tippy.js";
+import tippy, { animateFill } from "tippy.js";
 
 function tooltip_init() {
   //delete tooltips;
-  //console.log("tooltip_init");
+  console.log("tooltip_init");
   var tooltips = $(".tooltip").not("[data-tippy]");
   var tooltip_delay, tooltip_theme;
   if (tooltips.length) {
     tooltips.each(function () {
+      console.log("tooltip_init Item", $(this).get(0));
       if ($(this).hasClass("html")) {
         tooltip_delay = 0;
         tooltip_theme = "custom-html";
@@ -15,6 +16,8 @@ function tooltip_init() {
         tooltip_theme = "custom";
       }
       tippy($(this).get(0), {
+        plugins: [animateFill],
+        content: reference => reference.getAttribute("title"),
         duration: [
           window["scroll_duration"] / 2,
           window["scroll_duration"] / 4
@@ -22,19 +25,33 @@ function tooltip_init() {
         delay: [tooltip_delay, tooltip_delay / 2],
         //,arrow: true
         maxWidth: "30em",
-        updateDuration: window["scroll_duration"] / 4,
+        ////updateDuration: window["scroll_duration"] / 4,
+        moveTransition: "transform " + tooltip_delay + "s ease-out",
         theme: tooltip_theme,
         animation: "shift-away",
         animateFill: false,
         inertia: true,
         placement: "bottom",
-        flipBehavior: "flip",
         //,trigger: "click"
         //,interactive: true
-        dynamicTitle: true,
+        ////dynamicTitle: true,
         //,followCursor: true
-        touchHold: true,
-        hideOnClick: true
+        ////touchHold: true,
+        touch: "hold",
+        hideOnClick: true,
+        popperOptions: {
+          modifiers: [
+            {
+              name: "flip",
+              // flip: false
+              enabled: false,
+              options: {
+                // flipBehavior: ['bottom', 'right', 'top']
+                fallbackPlacements: ["right", "top"]
+              }
+            }
+          ]
+        }
       });
     });
   }
