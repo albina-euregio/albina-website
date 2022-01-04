cb1c421f91f5ce95484afceb05c8a0c7e1f0e164
Simon Legner <Simon.Legner@gmail.com>
2022-01-04 09:47:18 +0100

    feat(lola-safety): detailedPdf

--- src/app/observations/models/lola-safety.model.ts
+++ src/app/observations/models/lola-safety.model.ts
@@ -1,7 +1,6 @@
 import { convertLoLaToGeneric, LolaSnowProfile as SnowProfile } from "./lola-kronos.model";
 import {
   GenericObservation,
-  ObservationTableRow,
   ObservationSource,
 } from "./generic-observation.model";
 
@@ -40,6 +39,11 @@ export interface AvalancheReport {
   publicPdfGeneratedTime: string;
   latitude?: number;
   longitude?: number;
+  detailedPdf: string;
+  detailedPdfFinish: boolean;
+  detailedPdfToken: string;
+  detailedPdfTokenTime: string;
+  detailedPdfGeneratedTime: string;
 }
 
 export interface AvalanchePotential {
@@ -107,7 +111,9 @@ function convertAvalancheReport(
 ): GenericObservation<AvalancheReport> {
   return {
     $data: report,
-    $extraDialogRows: (t) => toLoLaTable(report, t),
+    $externalURL:
+      "https://www.lola-safety.info/api/file/avalancheReport/" +
+      (report.detailedPdf ?? report.publicPdf),
     $source: ObservationSource.LoLaSafetyAvalancheReports,
     aspect: undefined,
     authorName: report.firstName + " " + report.lastName,
@@ -119,38 +125,4 @@ function convertAvalancheReport(
     longitude: report.longitude,
     region: undefined
   };
-}
-
-export function toLoLaTable(report: AvalancheReport, t: (key: string) => string): ObservationTableRow[] {
-  return [
-    { label: t("LoLaAvalanchePotential.riskAssessment"), number: report.avalanchePotential.riskAssessment },
-    { label: t("LoLaAvalanchePotential.snowpackStructure"), number: report.avalanchePotential.snowpackStructure },
-    { label: t("LoLaAvalanchePotential.avalanchePotentialValue"), number: report.avalanchePotential.avalanchePotentialValue },
-    { label: t("LoLaAvalanchePotential.avalancheCharacteristicString"), value: report.avalanchePotential.avalancheCharacteristicString },
-    { label: t("avalancheProblem.windDriftedSnow"), value: formatAvalancheSituation(report.avalanchePotential.windDriftedSnow) },
-    { label: t("avalancheProblem.persistentWeakLayers"), value: formatAvalancheSituation(report.avalanchePotential.persistentWeakLayer) },
-    { label: t("avalancheProblem.wetSnow"), value: formatAvalancheSituation(report.avalanchePotential.wetSnow) },
-    { label: t("avalancheProblem.glidingSnow"), value: formatAvalancheSituation(report.avalanchePotential.glidingSnow) },
-    { label: t("LoLaAvalanchePotential.newSnow"), value: formatAvalancheSituation(report.avalanchePotential.newSnow) },
-    { label: t("LoLaAvalanchePotential.snowpackStructureActive"), boolean: report.avalanchePotential.snowpackStructureActive },
-    { label: t("LoLaAvalanchePotential.triggeringProbability"), value: report.avalanchePotential.triggeringProbability },
-    { label: t("observations.avalancheSize"), value: report.avalanchePotential.avalancheSize },
-    { label: t("observations.avalancheType"), value: report.avalanchePotential.avalancheType },
-    { label: t("LoLaAvalanchePotential.avalancheRiskTendency24h"), value: report.avalanchePotential.avalancheRiskTendency24h },
-    { label: t("LoLaAvalanchePotential.avalancheRiskTendency48h"), value: report.avalanchePotential.avalancheRiskTendency48h },
-    { label: t("observations.comment"), value: report.avalanchePotential.comment },
-    { label: t("LoLaAvalanchePotential.oldSnowFloatingSnow"), boolean: report.avalanchePotential.oldSnowFloatingSnow },
-    { label: t("LoLaAvalanchePotential.oldSnowSleet"), boolean: report.avalanchePotential.oldSnowSleet },
-    { label: t("LoLaAvalanchePotential.oldSnowWeakLayers"), boolean: report.avalanchePotential.oldSnowWeakLayers },
-    { label: t("LoLaAvalanchePotential.wetSnowRain"), boolean: report.avalanchePotential.wetSnowRain },
-    { label: t("LoLaAvalanchePotential.wetSnowTemperatureIncrease"), boolean: report.avalanchePotential.wetSnowTemperatureIncrease },
-    { label: t("LoLaAvalanchePotential.wetSnowHighRadiation"), boolean: report.avalanchePotential.wetSnowHighRadiation },
-    ...Object.keys(report.weather)
-      .map((key) => ({ label: t("LoLaWeather." + key), value: report.weather[key] }))
-      .filter(({ value }) => typeof value === "string")
-  ];
-}
-
-function formatAvalancheSituation(situation: AvalancheSituation): string {
-  return situation.aspects?.join(", ");
 }
