62cbbb42d9ee4c7e7ec5241d54b58d1afc133119
Simon Legner <Simon.Legner@gmail.com>
2022-01-11 12:53:45 +0100

    fix(lint)

--- src/app/shared/breadcrumb.component.ts
+++ src/app/shared/breadcrumb.component.ts
@@ -6,7 +6,7 @@ import "rxjs/add/operator/filter";
   selector: "app-breadcrumbs",
   template: `
   <ng-template ngFor let-breadcrumb [ngForOf]="breadcrumbs" let-last = last>
-    <li class="breadcrumb-item" *ngIf="breadcrumb.label.title&&breadcrumb.url.substring(breadcrumb.url.length-1) == '/' || breadcrumb.label.title&&last" [ngClass]="{active: last}">
+    <li class="breadcrumb-item" *ngIf="breadcrumb.label.title&&breadcrumb.url.substring(breadcrumb.url.length-1) === '/' || breadcrumb.label.title&&last" [ngClass]="{active: last}">
       <a *ngIf="!last" [routerLink]="breadcrumb.url">{{breadcrumb.label.title}}</a>
       <span *ngIf="last" [routerLink]="breadcrumb.url">{{breadcrumb.label.title}}</span>
     </li>
