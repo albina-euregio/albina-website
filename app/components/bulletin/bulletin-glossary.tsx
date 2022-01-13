3dc6b43476c60b67f19d0a691ddb9f07fcfa3f00
Simon Legner <Simon.Legner@gmail.com>
2022-01-13 12:54:32 +0100

    feat(glossary): include glossary content
    https://gitlab.com/eaws/eaws-glossary-parser

--- app/components/bulletin/bulletin-glossary.tsx
+++ app/components/bulletin/bulletin-glossary.tsx
@@ -3,7 +3,16 @@ import React from "react";
 
 import "tippy.js/dist/tippy.css";
 
+import { preprocessContent } from "../../util/htmlParser";
 import GLOSSARY_LINKS from "./bulletin-glossary-links.json";
+import GLOSSARY_CONTENT from "./bulletin-glossary-content.json";
+
+console.warn(
+  "Missing glossary",
+  Array.from(
+    new Set(Object.values(GLOSSARY_LINKS).filter(id => !GLOSSARY_CONTENT[id]))
+  ).join(" ")
+);
 
 export function findGlossaryStrings(text: string): string {
   return Object.entries(GLOSSARY_LINKS).reduce((e, [re, glossary]) => {
@@ -22,8 +31,10 @@ type Props = {
 export default class BulletinGlossary extends React.Component<Props> {
   render() {
     const glossary = this.props.glossary;
+    const text = GLOSSARY_CONTENT[glossary]?.text;
+    const content = text ? preprocessContent(text) : "???";
     return (
-      <Tippy content={glossary}>
+      <Tippy content={content}>
         <a
           className="glossary"
           href={`https://www.avalanches.org/glossary/?lang=de#${glossary}`}
