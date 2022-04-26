072d300a4a891bfc0a7933f6a11b8b58b3d4bd91
TWanner <wanner@transporter.at>
2022-04-26 16:46:30 +0200

    setup local filter

--- src/app/observations/observations-local-filter/observations-local-filter.component.ts
+++ src/app/observations/observations-local-filter/observations-local-filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ObservationLocalFilterService } from "./observation-local-filter.service";
+import { ObservationFilterService } from "../observation-filter.service";
 
 @Component({
   selector: 'app-observations-local-filter',
@@ -7,39 +8,31 @@ import { ObservationLocalFilterService } from "./observation-local-filter.servic
 })
 export class ObservationsLocalFilterComponent implements OnInit {
 
-  @Input() filter: ObservationLocalFilterService;
 
   public barChartData: Object = {};
   public roseChartData: Object = {};
 
-  constructor(public localFilter: ObservationLocalFilterService) { 
-    this.barChartData = {'dataset': 
-      {'source': [
-        ['category', 'max', 'all', 'selected', 'highlighted'],
-        ['1000', 100, 90, 20, 0],
-        ['1500', 100, 90, 20, 0],
-        ['2000', 100, 90, 20, 0],
-        ['2500', 100, 80, 0, 60],
-        ['3000', 100, 80, 0, 70],
-        ['3500', 100, 50, 30, 0],
-        ]
-      }
-    };
-    this.roseChartData = {dataset: {
-      // Provide a set of data.
-      source: [
-            ['category', 'all','selected', 'highlighted'],
-            ['N', 100, 20, 0],
-            ['NE', 70, 0, 60],
-            ['E', 30, 0, 20],
-            ['SE', 80, 80, 0],
-            ['S', 90, 80, 0],
-            ['SW', 100, 0, 30],
-            ['W', 80, 80, 0],
-            ['NW', 90, 80, 0],
-        ]
-      }
-    };
+  //@Input() filter: ObservationFilterService;
+
+  @Input() 
+  set filter(filter: ObservationFilterService) {
+    //if(filter) this.localFilter.setGlobalFilter(filter);
+    this._filter = filter;
+    this.localFilterService.setGlobalFilter(filter);
+    this.barChartData = this.localFilterService.getBarData();
+    this.roseChartData = this.localFilterService.getRoseData();
+    
+  }
+  @Input() 
+  set observations(observations: Array<Object>) {
+    console.log("ObservationsLocalFilterComponent->set observations", observations);
+    this._observations = observations;  
+  }
+  private _filter = {};
+  private _observations = {};
+
+  constructor(public localFilterService: ObservationLocalFilterService) { 
+    
 
   }
 
