a939a2566c91b7d8a8b33d25ab20d05680aab7b9
Simon Legner <Simon.Legner@gmail.com>
2022-10-24 20:26:27 +0200

    feat: Load environment variables via assets/env.js
    https://pumpingco.de/blog/environment-variables-angular-docker/

---
+++ src/assets/env.js
@@ -0,0 +1,8 @@
+(function (window) {
+  window.ENV = {
+    apiBaseUrl: "/albina/api/",
+    wsBaseUrl: "wss://socket.avalanche.report/albina/",
+    textcatUrl: "/textcat-ng/",
+    headerBgColor: "#f95bdc"
+  };
+})(this);
