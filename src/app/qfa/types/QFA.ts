8627f9ae36ba46b3459c44c92684bc552b1d4970
FloerlPhilip <floerl.philip@gmail.com>
2022-11-17 13:55:37 +0100

    initial commit & fixed typings

---
+++ src/app/qfa/types/QFA.ts
@@ -0,0 +1,60 @@
+export interface coordinates {
+    lat: number;
+    lon: number;
+}
+
+export interface metadata {
+    location: string;
+    coords: coordinates;
+    height: number;
+    orog: number;
+    date: Date;
+    timezone: string;
+    model: string;
+    days: number
+}
+
+export interface parameters {
+    [key: string]: {
+        [key: string]: any;
+    }
+}
+
+export interface data {
+    metadata: metadata;
+    parameters: parameters;
+}
+
+export interface QFA {
+    data: data;
+    coordinates: coordinates;
+    height: number;
+    metadata: metadata;
+    parameters: parameters;
+    filename: string;
+    loadFromJSON: (filename: string) => boolean;
+    loadFromFile: (filename: string) => void;
+    loadFromURL: (url: string) => Promise<void>;
+    listParameters(): string[];
+    dump(filename?: string): void;
+}
+
+export interface marker {
+    coordinates: coordinates;
+    filename: string;
+}
+
+export interface markers {
+    [key: string]: {
+        coordinates: coordinates;
+        filenames: string[];
+    }
+}
+
+export interface MarkerData {
+    data: markers;
+    coordinates: coordinates[];
+    add: (marker: marker) => void;
+    getFilenames: (coordinates: coordinates) => string[];
+    save: () => void;
+}
