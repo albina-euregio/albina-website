9ca7f4b07d9c647662915d33306d7dc8080d4c64
FloerlPhilip <floerl.philip@gmail.com>
2022-11-22 20:45:09 +0100

    very beautiful colors, evaluated by experts

--- src/app/qfa/qfa.component.ts
+++ src/app/qfa/qfa.component.ts
@@ -80,8 +80,8 @@ export class QfaComponent implements AfterViewInit, OnDestroy {
   }
 
   private getCellClass(param: string, value: string): string {
-    const nParams = ["N", "Nh", "Nm", "Nl", "N_CU"];
-    if(nParams.includes(this.parameterClasses[param])) {
+    let params = ["N", "Nh", "Nm", "Nl", "N_CU"];
+    if(params.includes(this.parameterClasses[param])) {
       const parsedVal = Number(value);
       if (parsedVal >= 7) return "darkest";
       if (parsedVal >= 5) return "darker";
@@ -89,7 +89,52 @@ export class QfaComponent implements AfterViewInit, OnDestroy {
       if (parsedVal == 0) return "opacity-0";
     }
 
+    params = ["RF_300", "RF_400", "RF_500", "RF_700", "RF_850", "RF_925"];
+    if(params.includes(this.parameterClasses[param])) {
+      const parsedVal = Number(value);
+      if (parsedVal >= 90) return "darkest";
+      if (parsedVal >= 80) return "darker";
+      if (parsedVal >= 70) return "dark";
+    }
+
+    params = ["WX_CUF"];
+    if(params.includes(this.parameterClasses[param])) {
+      if (value === "NIL") return "opacity-0";
+      if (value === "TS") return "yellow";
+      if (value === "XXTS") return "orange";
+    }
+
+    params = ["SN_RA"];
+    if(params.includes(this.parameterClasses[param])) {
+      const parsedVal = Number(value);
+      if(parsedVal <= 1000) return "sn-rf-color";
+    }
+
+    params = ["RR, RR_24h, SN"];
+    if(params.includes(this.parameterClasses[param])) {
+      if(value === "0.0" || value === "---") return "opacity-0";
+    }
+
+    params = ["QANmax"];
+    if(params.includes(this.parameterClasses[param])) {
+      const parsedVal = Number(value);
+      if(parsedVal >= 15) return "orange";
+    }
+
     return "";
+  }
+
+  private getWParam(value: string): string {
+    const parsedVal = Number(value);
+    if (parsedVal <= -1000) return `-----${value}`;
+    if (parsedVal <= -25) return `---${value}`;
+    if (parsedVal <= -9) return `--${value}`;
+    if (parsedVal <= -3) return `-${value}`;
+    if (parsedVal <= 3) return value;
+    if (parsedVal <= 15) return `+${value}`;
+    if (parsedVal <= 30) return `++${value}`;
+    if (parsedVal <= 100) return `+++${value}`;
+    return `++++${value}`;
   }
 
   private async showRun(run) {
