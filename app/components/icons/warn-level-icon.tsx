b38d7592980e5374e4a99d4f86ac39e684ddce77
Simon Legner <Simon.Legner@gmail.com>
2022-11-23 22:27:29 +0100

    refact: transform warning-pictos to SVG

--- app/components/icons/warn-level-icon.tsx
+++ app/components/icons/warn-level-icon.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { injectIntl } from "react-intl";
 import { Link } from "react-router-dom";
 import { APP_STORE } from "../../appStore";
-import { getWarnlevelNumber } from "../../util/warn-levels";
+import { getWarnlevelNumber, WARNLEVEL_COLORS } from "../../util/warn-levels";
 import { Tooltip } from "../../components/tooltips/tooltip";
 
 class WarnLevelIcon extends React.Component {
@@ -37,6 +37,10 @@ class WarnLevelIcon extends React.Component {
 
     const numberBelow = getWarnlevelNumber(below);
     const numberAbove = getWarnlevelNumber(this.props.above);
+    const colorBelow = WARNLEVEL_COLORS[numberBelow];
+    const colorAbove = WARNLEVEL_COLORS[numberAbove];
+    const colorBelowText = numberBelow >= 4 ? "#FFF" : "#222";
+    const colorAboveText = numberAbove >= 4 ? "#FFF" : "#222";
 
     const imgFormat = window.config.webp ? ".webp" : ".png";
     const img =
@@ -84,10 +88,102 @@ class WarnLevelIcon extends React.Component {
       elevationText = this.props.elevation + "m";
     }
 
+    const svg = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 65 48"
+      >
+        <defs>
+          <path
+            id="p0"
+            d="M63.434 47.25A.5.5 0 0 1 63 48H1a.5.5 0 0 1-.432-.751l25-43a.5.5 0 0 1 .866.003l3.556 6.222L35.56.261a.5.5 0 0 1 .873-.01l27 47Z"
+          />
+          <path id="p1" d="M11.334 0h39.462l10.34 18H.87z" />
+          <path
+            id="p2"
+            d="M.95 27 14.03 4.501l3.57 6.247a.5.5 0 0 0 .873-.009L24.046.523 39.256 27H.95Z"
+          />
+        </defs>
+        {numberAbove === numberBelow ? (
+          <g fill="none" fillRule="evenodd">
+            <mask id="m0" fill="#fff">
+              <use xlinkHref="#p0" />
+            </mask>
+            <use xlinkHref="#p0" fill="#FFF" />
+            <g fill={colorBelow} mask="url(#m0)">
+              <path d="M0-1h65v50H0z" />
+            </g>
+            <text
+              fill={colorBelowText}
+              fontSize="12"
+              fontWeight="bold"
+              letterSpacing=".5"
+              mask="url(#m0)"
+            >
+              <tspan x="28.119" y="35">
+                {numberBelow}
+              </tspan>
+            </text>
+            <path stroke="#222" d="M63 47.5 36 .5l-6 11-4-7-25 43h62Z" />
+          </g>
+        ) : (
+          <g fill="none" fillRule="evenodd">
+            <path
+              fill="#222"
+              d="M51.875 28h12.626v1H52.45l10.484 18.25a.5.5 0 0 1-.433.75H.5a.5.5 0 0 1-.433-.751l25-43a.5.5 0 0 1 .867.003l3.556 6.222L35.06.261a.5.5 0 0 1 .873-.01L51.875 28Z"
+            />
+            <g transform="translate(.5 29)">
+              <mask id="m1" fill="#fff">
+                <use xlinkHref="#p1" />
+              </mask>
+              <g fill={colorBelow} mask="url(#m1)">
+                <path d="M-1-30h65v50H-1z" />
+              </g>
+              <text
+                fill={colorBelowText}
+                fontSize="12"
+                fontWeight="bold"
+                letterSpacing=".5"
+                mask="url(#m1)"
+              >
+                <tspan x="27.119" y="14">
+                  {numberBelow}
+                </tspan>
+              </text>
+            </g>
+            <g transform="translate(11.5 1)">
+              <mask id="m2" fill="#fff">
+                <use xlinkHref="#p2" />
+              </mask>
+              <g fill={colorAbove} mask="url(#m2)">
+                <path d="M-12-2h65v50h-65z" />
+              </g>
+              <text
+                fill={colorAboveText}
+                fontSize="12"
+                fontWeight="bold"
+                letterSpacing=".5"
+                mask="url(#m2)"
+              >
+                <tspan x="17.171" y="23">
+                  {numberAbove}
+                </tspan>
+              </text>
+            </g>
+          </g>
+        )}
+      </svg>
+    );
+
     return (
       <Tooltip label={title}>
         <Link to={link} tabIndex="-1">
-          <img src={img} alt={title} />
+          {numberAbove === 5 || numberBelow === 5 ? (
+            <img src={img} alt={title} />
+          ) : (
+            svg
+          )}
           {this.props.above != this.props.below && <span>{elevationText}</span>}
         </Link>
       </Tooltip>
