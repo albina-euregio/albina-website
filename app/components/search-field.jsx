98386650e2acd05f6fde0464d04f8d365333d62e
Simon Legner <Simon.Legner@gmail.com>
2022-12-03 00:42:19 +0100

    refact(search-field): functional components

--- app/components/search-field.jsx
+++ app/components/search-field.jsx
@@ -1,71 +1,38 @@
-import React from "react";
-import { injectIntl } from "react-intl";
+import React, { useState } from "react";
+import { useIntl } from "react-intl";
 import { Tooltip } from "./tooltips/tooltip";
 
-class SearchField extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: ""
-    };
-  }
-
-  componentDidMount() {
-    if (this.props.value) {
-      this.setState({ text: this.props.value });
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    if (
-      typeof this.props.value !== "undefined" &&
-      this.props.value != prevProps.value &&
-      this.props.value != this.state.text
-    ) {
-      this.setState({ text: this.props.value });
-    }
-  }
-
-  render() {
-    const placeholder = this.props.intl.formatMessage({ id: "filter:search" });
-    return (
-      <div>
-        <p className="info">{this.props.title}</p>
-        <div className="pure-form pure-form-search">
-          <input
-            type="text"
-            id="input"
-            placeholder={placeholder}
-            onChange={e => {
-              const newVal = e.target.value;
-              if (newVal != this.state.text) {
-                window.setTimeout(() => {
-                  this.setState({ text: newVal });
-                }, 0);
-              }
-            }}
-            onKeyPress={e => {
-              if (e.key == "Enter") {
-                this.props.handleSearch(this.state.text);
-              }
-            }}
-            value={this.state.text}
-          />
-          <Tooltip label={placeholder}>
-            <button
-              href="#"
-              aria-label={placeholder}
-              className="pure-button pure-button-icon icon-search"
-              onClick={() => {
-                this.props.handleSearch(this.state.text);
-              }}
-            >
-              <span>&nbsp;</span>
-            </button>
-          </Tooltip>
-        </div>
+export default function SearchField({ value, title, handleSearch }) {
+  const intl = useIntl();
+  const [text, setText] = useState(value || "");
+  const placeholder = intl.formatMessage({ id: "filter:search" });
+  return (
+    <div>
+      <p className="info">{title}</p>
+      <div className="pure-form pure-form-search">
+        <input
+          type="text"
+          id="input"
+          placeholder={placeholder}
+          onChange={e => setText(e.target.value)}
+          onKeyPress={e => {
+            if (e.key == "Enter") {
+              handleSearch(text);
+            }
+          }}
+          value={text}
+        />
+        <Tooltip label={placeholder}>
+          <button
+            href="#"
+            aria-label={placeholder}
+            className="pure-button pure-button-icon icon-search"
+            onClick={() => handleSearch(text)}
+          >
+            <span>&nbsp;</span>
+          </button>
+        </Tooltip>
       </div>
-    );
-  }
+    </div>
+  );
 }
-export default injectIntl(SearchField);
