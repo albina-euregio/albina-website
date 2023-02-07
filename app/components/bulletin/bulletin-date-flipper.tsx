be051ed7e8e9852082afc26a10cbfa35054ccc3f
TWanner <wanner@transporter.at>
2023-02-07 14:05:44 +0100

    add calendar picker

--- app/components/bulletin/bulletin-date-flipper.tsx
+++ app/components/bulletin/bulletin-date-flipper.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useIntl } from "react-intl";
 import {
   getPredDate,
@@ -9,11 +9,15 @@ import {
   isAfter
 } from "../../util/date.js";
 import { Tooltip } from "../tooltips/tooltip";
+import "react-calendar/dist/Calendar.css";
+import Calendar from "react-calendar";
 
 type Props = { date: Date; latest: Date };
 
 function BulletinDateFlipper({ date, latest }: Props) {
   const intl = useIntl();
+  const [showCalendar, setShowCalender] = useState(false);
+  const navigate = useNavigate();
   const date$: Date | null = date && +date ? date : null;
   let nextDate$: Date | null = null;
   if (date$ && latest) {
@@ -21,75 +25,102 @@ function BulletinDateFlipper({ date, latest }: Props) {
     /* show next day only it is not the future or if this day is after bulletin.isTomorrow value */
     nextDate$ = isSameDay(latest, next) || isAfter(latest, next) ? next : null;
   }
+
   const prevDate$: Date | null = date$ ? getPredDate(date$) : null;
   const prevLink = dateToISODateString(prevDate$);
   const nextLink = dateToISODateString(nextDate$);
   const prevDate = prevDate$ ? intl.formatDate(prevDate$) : "";
   const nextDate = nextDate$ ? intl.formatDate(nextDate$) : "";
 
+  const onChangeCurrentDate = (newDate: Date) => {
+    if (newDate) {
+      setShowCalender(!showCalendar);
+      navigate("/bulletin/" + dateToISODateString(newDate));
+    }
+  };
+
   return (
-    <ul className="list-inline bulletin-flipper">
-      <li className="bulletin-flipper-back">
-        <Tooltip
-          label={intl.formatMessage({
-            id: "bulletin:header:dateflipper:back"
-          })}
-        >
-          <Link to={"/bulletin/" + prevLink}>
-            <span className="icon-arrow-left" />
-            {prevDate}
-          </Link>
-        </Tooltip>
-      </li>
-      <li className="bulletin-flipper-calendar">
-        <a href="#" title="Calendar" className="tooltip">
-          <span className="icon-calendar"></span>
-        </a>
-      </li>
-      {/* {nextLink && <li className="bulletin-flipper-separator">&nbsp;</li>} */}
-      {nextLink && (
-        <li className="bulletin-flipper-forward">
+    <>
+      {showCalendar && (
+        <Calendar
+          onChange={onChangeCurrentDate}
+          maxDate={latest}
+          value={date$}
+        />
+      )}
+      <ul className="list-inline bulletin-flipper">
+        <li className="bulletin-flipper-back">
           <Tooltip
             label={intl.formatMessage({
-              id: "bulletin:header:dateflipper:forward"
+              id: "bulletin:header:dateflipper:back"
             })}
           >
-            <Link to={"/bulletin/" + nextLink}>
-              {nextDate + " "}
-              <span className="icon-arrow-right" />
+            <Link to={"/bulletin/" + prevLink}>
+              <span className="icon-arrow-left" />
+              {prevDate}
             </Link>
           </Tooltip>
         </li>
-      )}
-      {nextDate && (
-        <li className="bulletin-flipper-latest">
+        <li className="bulletin-flipper-calendar">
           <Tooltip
             label={intl.formatMessage({
-              id: "bulletin:header:dateflipper:latest:hover"
+              id: "bulletin:header:dateflipper:showHideCalendar"
             })}
           >
-            <Link to={"/bulletin/" + (latest || "")}>
+            <a
+              href="#"
+              title="Calendar"
+              onClick={() => setShowCalender(!showCalendar)}
+            >
+              <span className="icon-calendar"></span>
+            </a>
+          </Tooltip>
+        </li>
+        {/* {nextLink && <li className="bulletin-flipper-separator">&nbsp;</li>} */}
+        {nextLink && (
+          <li className="bulletin-flipper-forward">
+            <Tooltip
+              label={intl.formatMessage({
+                id: "bulletin:header:dateflipper:forward"
+              })}
+            >
+              <Link to={"/bulletin/" + nextLink}>
+                {nextDate + " "}
+                <span className="icon-arrow-right" />
+              </Link>
+            </Tooltip>
+          </li>
+        )}
+        {nextDate && (
+          <li className="bulletin-flipper-latest">
+            <Tooltip
+              label={intl.formatMessage({
+                id: "bulletin:header:dateflipper:latest:hover"
+              })}
+            >
+              <Link to={"/bulletin/" + (latest || "")}>
+                {intl.formatMessage({
+                  id: "bulletin:header:dateflipper:latest"
+                })}
+              </Link>
+            </Tooltip>
+          </li>
+        )}
+        <li className="bulletin-flipper-archive">
+          <Tooltip
+            label={intl.formatMessage({
+              id: "bulletin:header:archive:hover"
+            })}
+          >
+            <Link to="/more/archive">
               {intl.formatMessage({
-                id: "bulletin:header:dateflipper:latest"
+                id: "bulletin:header:archive"
               })}
             </Link>
           </Tooltip>
         </li>
-      )}
-      <li className="bulletin-flipper-archive">
-        <Tooltip
-          label={intl.formatMessage({
-            id: "bulletin:header:archive:hover"
-          })}
-        >
-          <Link to="/more/archive">
-            {intl.formatMessage({
-              id: "bulletin:header:archive"
-            })}
-          </Link>
-        </Tooltip>
-      </li>
-    </ul>
+      </ul>
+    </>
   );
 }
 
