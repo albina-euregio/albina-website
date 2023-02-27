fe998d831ad71e50a7e6c1fbf17ea04a05823fb1
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2023-02-27 15:44:29 +0100

    fix(audio-file): disable a11y for synthesized-bulletin

--- app/components/bulletin/synthesized-bulletin.tsx
+++ app/components/bulletin/synthesized-bulletin.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/media-has-caption */
 import React from "react";
 import { useState, useEffect } from "react";
 import { observer } from "mobx-react";
