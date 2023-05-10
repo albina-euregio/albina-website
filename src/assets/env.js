9ad15039fd89e4eea8af075c3940950c82d81023
Thomas Wanner <wanner@transporter.at>
2023-05-10 09:33:49 +0200

    fix spelling

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,8 +1,8 @@
 (function (window) {
   window["env"] = window["env"] || {};
-  window["ENV"].PROPDUCTION = false,
+  window["env"].PRODUCTION = false,
   window["env"].API_BASE_URL = "https://api.avalanche.report/albina_dev/api/",
   window["env"].WS_BASE_URL = "wss://socket.avalanche.report/albina/",
   window["env"].TEXTCAT_URL= "/textcat-ng/",
-  window["ENV"].SHOW_CHAT= false
+  window["env"].SHOW_CHAT= false
 })(this);
