71d1a4e9c5ef823130db0e3f1b43a2b9435e5ae6
Simon Legner <Simon.Legner@gmail.com>
2023-12-28 13:54:39 +0100

    refact(subscribe-email-dialog): functional components

--- app/components/dialogs/subscribe-email-dialog.jsx
+++ app/components/dialogs/subscribe-email-dialog.jsx
@@ -1,239 +1,191 @@
-import React from "react";
-import { FormattedMessage, injectIntl } from "react-intl";
+import React, { useCallback, useState } from "react";
+import { FormattedMessage, useIntl } from "react-intl";
 import { Link } from "react-router-dom";
 
-import LanguageFilter from "../filters/language-filter";
 import ProvinceFilter from "../filters/province-filter";
+import LanguageFilter from "../filters/language-filter";
 import { fetchText } from "../../util/fetch";
 
-class SubscribeEmailDialog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+export default function SubscribeEmailDialog() {
+  const intl = useIntl();
+  const lang = intl.locale.slice(0, 2);
+  const [language, setLanguage] = useState(lang);
+  const [region, setRegion] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const [agree, setAgree] = useState(false);
+  const [status, setStatus] = useState("");
 
-  resetState() {
-    this.setState({
-      email: "",
-      language: this.props.intl.locale.slice(0, 2),
-      region: false,
-      status: "",
-      errorMessage: "",
-      agree: false
-    });
-  }
-
-  componentDidMount() {
-    this.resetState();
-  }
-
-  componentDidUpdate() {
-    // reset on dialog close after form has been submitted
-    if (this.state.status !== "") {
-      this.resetState();
-    }
-  }
-
-  handleChangeEmail = e => {
-    const value = e.target.value;
-
-    if (this.validateEmail(value)) {
-      this.setState({ email: value });
-    } else if (this.state.email) {
-      this.setState({ email: "" });
-    }
-  };
-
-  handleChangeAgree = e => {
-    this.setState({ agree: e.target.checked });
-  };
-
-  handleChangeLanguage = language => {
-    this.setState({ language });
-  };
-
-  handleChangeRegion = newRegion => {
-    this.setState({ region: newRegion !== "none" ? newRegion : false });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    e.stopPropagation();
-
+  const handleSubmit = useCallback(async () => {
     const data = {
-      language: this.state.language,
-      regions: this.state.region,
-      email: this.state.email
+      language: language,
+      regions: region,
+      email: email
     };
 
-    this.setState({ status: "loading" });
-    fetchText(config.apis.subscribe + "/subscribe", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    }).then(
-      () => {
-        this.setState({ status: "submitted" });
-      },
-      error => {
-        this.setState({ errorMessage: error });
-      }
-    );
-  };
+    setStatus("loading");
+    try {
+      await fetchText(config.apis.subscribe + "/subscribe", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+      setStatus("submitted");
+    } catch (reason) {
+      setErrorMessage(String(reason));
+      throw reason;
+    }
+  }, [setErrorMessage, email, language, region]);
 
-  validate() {
-    return (
-      this.state.email !== "" &&
-      this.state.language !== "" &&
-      this.state.region !== false &&
-      this.state.agree
-    );
-  }
-
-  validateEmail(email) {
+  function validateEmail(email) {
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
   }
 
-  render() {
-    // add a dummy class to react to close events
-    //const isOpen = window["modalStateStore"].isOpen ? "" : " closed";
-    return (
-      <div className="modal-subscribe-email">
-        <div className="modal-header">
-          <h2>
-            <FormattedMessage id="dialog:subscribe-email:subheader" />
-          </h2>
-        </div>
+  return (
+    <div className="modal-subscribe-email">
+      <div className="modal-header">
+        <h2>
+          <FormattedMessage id="dialog:subscribe-email:subheader" />
+        </h2>
+      </div>
 
-        {!this.state.status && (
-          <form
-            className="pure-form pure-form-stacked"
-            onSubmit={this.handleSubmit}
+      {!status && (
+        <form
+          className="pure-form pure-form-stacked"
+          onSubmit={event => {
+            event.preventDefault();
+            event.stopPropagation();
+            handleSubmit();
+          }}
+        >
+          <label htmlFor="province">
+            <FormattedMessage
+              id="dialog:subscribe-email:region"
+              values={{
+                strong: (...msg) => <strong>{msg}</strong>
+              }}
+            />
+          </label>
+          <ProvinceFilter
+            buttongroup={true}
+            title={intl.formatMessage({
+              id: "measurements:filter:province"
+            })}
+            handleChange={r => setRegion(r !== "none" ? r : false)}
+            value={region}
+            none={intl.formatMessage({
+              id: "blog:filter:province:nothing-selected"
+            })}
+          />
+
+          <label htmlFor="language">
+            <FormattedMessage id="dialog:subscribe-email:language" />
+          </label>
+          <LanguageFilter
+            buttongroup={true}
+            title={intl.formatMessage({
+              id: "measurements:filter:province"
+            })}
+            handleChange={l => setLanguage(l)}
+            value={language}
+          />
+
+          <hr />
+
+          <p>
+            <label htmlFor="email">
+              <FormattedMessage id="dialog:subscribe-email:email" />
+              <span className="normal" />
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              className="full-width"
+              onChange={e => {
+                const value = e.target.value;
+                if (validateEmail(value)) {
+                  setEmail(value);
+                } else if (email) {
+                  setEmail("");
+                }
+              }}
+              placeholder={intl.formatMessage({
+                id: "dialog:subscribe-email:email"
+              })}
+            />
+          </p>
+
+          <hr />
+
+          <p className="">
+            <label htmlFor="agree">
+              <input
+                id="agree"
+                type="checkbox"
+                onChange={e => setAgree(e.target.checked)}
+                checked={!!agree}
+              />
+              &nbsp; &nbsp;
+              {intl.formatMessage({
+                id: "dialog:subscribe-email:subscribe:agree-before-link"
+              })}
+              <Link
+                title={intl.formatMessage({
+                  id: "dialog:subscribe-email:subscribe:agree-link"
+                })}
+                to="/more/privacy/"
+                target="_blank"
+              >
+                {intl.formatMessage({
+                  id: "dialog:subscribe-email:subscribe:agree-link"
+                })}
+              </Link>
+              {intl.formatMessage({
+                id: "dialog:subscribe-email:subscribe:agree-after-link"
+              })}
+            </label>
+          </p>
+
+          <button
+            type="submit"
+            className="pure-button"
+            disabled={email && region && language && agree ? "" : "disabled"}
           >
-            <label htmlFor="province">
+            {intl.formatMessage({
+              id: "dialog:subscribe-email:subscribe:button"
+            })}
+          </button>
+        </form>
+      )}
+
+      {(status || errorMessage) && agree && (
+        <div className="field-2 panel">
+          {status && (
+            <p className="status-message">
               <FormattedMessage
-                id="dialog:subscribe-email:region"
+                id={"dialog:subscribe-email:status:" + status}
                 values={{
                   strong: (...msg) => <strong>{msg}</strong>
                 }}
               />
-            </label>
-            <ProvinceFilter
-              buttongroup={true}
-              title={this.props.intl.formatMessage({
-                id: "measurements:filter:province"
-              })}
-              handleChange={r => this.handleChangeRegion(r)}
-              value={this.state.region}
-              none={this.props.intl.formatMessage({
-                id: "blog:filter:province:nothing-selected"
-              })}
-            />
-
-            <label htmlFor="language">
-              <FormattedMessage id="dialog:subscribe-email:language" />
-            </label>
-            <LanguageFilter
-              buttongroup={true}
-              title={this.props.intl.formatMessage({
-                id: "measurements:filter:province"
-              })}
-              handleChange={l => this.handleChangeLanguage(l)}
-              value={this.state.language}
-            />
-
-            <hr />
-
-            <p>
-              <label htmlFor="email">
-                <FormattedMessage id="dialog:subscribe-email:email" />
-                <span className="normal" />
-              </label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                className="full-width"
-                onChange={this.handleChangeEmail}
-                placeholder={this.props.intl.formatMessage({
-                  id: "dialog:subscribe-email:email"
-                })}
-              />
             </p>
-
-            <hr />
-
-            <p className="">
-              <label htmlFor="agree">
-                <input
-                  id="agree"
-                  type="checkbox"
-                  onChange={e => this.handleChangeAgree(e)}
-                  checked={!!this.state.agree}
-                />
-                &nbsp; &nbsp;
-                {this.props.intl.formatMessage({
-                  id: "dialog:subscribe-email:subscribe:agree-before-link"
+          )}
+          {errorMessage && (
+            <p className="status-message">
+              <strong className="error">
+                {intl.formatMessage({
+                  id: "dialog:subscribe-email:error"
                 })}
-                <Link
-                  title={this.props.intl.formatMessage({
-                    id: "dialog:subscribe-email:subscribe:agree-link"
-                  })}
-                  to="/more/privacy/"
-                  target="_blank"
-                >
-                  {this.props.intl.formatMessage({
-                    id: "dialog:subscribe-email:subscribe:agree-link"
-                  })}
-                </Link>
-                {this.props.intl.formatMessage({
-                  id: "dialog:subscribe-email:subscribe:agree-after-link"
-                })}
-              </label>
+              </strong>
+              &nbsp;{errorMessage}
             </p>
-
-            <button
-              type="submit"
-              className="pure-button"
-              disabled={this.validate() ? "" : "disabled"}
-            >
-              {this.props.intl.formatMessage({
-                id: "dialog:subscribe-email:subscribe:button"
-              })}
-            </button>
-          </form>
-        )}
-
-        {(this.state.status || this.state.errorMessage) && this.state.agree && (
-          <div className="field-2 panel">
-            {this.state.status && (
-              <p className="status-message">
-                <FormattedMessage
-                  id={"dialog:subscribe-email:status:" + this.state.status}
-                  values={{
-                    strong: (...msg) => <strong>{msg}</strong>
-                  }}
-                />
-              </p>
-            )}
-            {this.state.errorMessage && (
-              <p className="status-message">
-                <strong className="error">
-                  {this.props.intl.formatMessage({
-                    id: "dialog:subscribe-email:error"
-                  })}
-                  :
-                </strong>
-                &nbsp;{this.state.errorMessage}
-              </p>
-            )}
-          </div>
-        )}
-      </div>
-    );
-  }
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
-export default injectIntl(SubscribeEmailDialog);
