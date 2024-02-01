ff335bfd982f4146f60fb86d3f4952a23cee697c
Simon Legner <Simon.Legner@gmail.com>
2024-02-01 17:01:58 +0100

    chore(sentry): exclude leaflet reading '_leaflet_pos'

--- app/sentry.ts
+++ app/sentry.ts
@@ -3,6 +3,17 @@
 import * as Sentry from "@sentry/react";
 if (import.meta.env.PROD) {
   Sentry.init({
+    beforeSend(event) {
+      const exception = event.exception?.values?.[0];
+      if (
+        exception?.value?.includes("reading '_leaflet_pos'") ||
+        exception?.stacktrace?.frames?.some(
+          f => f.function?.includes("_onZoomTransitionEnd")
+        )
+      ) {
+        return null;
+      }
+    },
     release: "albina-website@" + import.meta.env.APP_VERSION,
     environment: import.meta.env.MODE,
     dsn: "https://513851e41d6e455998f0cc1a91828942@sentry.io/1819947"
