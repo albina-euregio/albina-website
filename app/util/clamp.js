460c1802cbe3f755799f77cc2906651e5d95e348
Simon Legner <Simon.Legner@gmail.com>
2024-07-28 14:28:08 +0200

    refact(clamp): convert to TypeScript

--- app/util/clamp.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number} num
- * @param {number} min
- * @param {number} max
- * @returns {number}
- */
-export function clamp(num, min, max) {
-  return num <= min ? min : num >= max ? max : num;
-}
