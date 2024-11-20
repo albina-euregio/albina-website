a8536faffee04ed8baf6b6bfa2aad81f9e33b52b
Simon Legner <Simon.Legner@gmail.com>
2024-11-20 10:33:15 +0100

    chore(sm-share): try navigator.share

--- app/components/organisms/sm-share.tsx
+++ app/components/organisms/sm-share.tsx
@@ -67,6 +67,29 @@ export default function SmShare(props: Props) {
         />
       </p>
       <ul className="list-inline sm-buttons">
+        <li>
+          <a
+            href={location.href}
+            onClick={e => {
+              e.preventDefault();
+              navigator.share({
+                url: location.href.toString(),
+                title: props.title,
+                text: props.description
+              });
+            }}
+          >
+            <svg
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-share"
+              viewBox="0 0 16 16"
+            >
+              <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3" />
+            </svg>
+          </a>
+        </li>
         {types.map(type => (
           <li key={type}>
             <Tooltip
