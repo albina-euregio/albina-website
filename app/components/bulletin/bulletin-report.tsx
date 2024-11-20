5c7e4f3680ff49e3606986d633d13b0fd69b51e4
Simon Legner <Simon.Legner@gmail.com>
2024-11-20 10:24:37 +0100

    chore(bulletin-report): try navigator.share

--- app/components/bulletin/bulletin-report.tsx
+++ app/components/bulletin/bulletin-report.tsx
@@ -225,7 +225,47 @@ function BulletinReport({ date, bulletin, bulletin170000 }: Props) {
             ) && (
               <div>
                 <ul className="list-inline list-buttongroup bulletin-report-header-download">
+                  {typeof navigator.share === "function" && ( 
                   <li>
+                    <Tooltip
+                      label={
+                        <FormattedMessage
+                          id="main:share-this"
+                          html={true}
+                          values={{
+                            strong: (...msg) => <strong>{msg}</strong>
+                          }}
+                        />
+                      }
+                    >
+                      <a
+                        href={"#" + bulletin.bulletinID}
+                        onClick={e => {
+                          e.preventDefault();
+                          navigator.share({
+                            url: new URL(
+                              "#" + bulletin.bulletinID,
+                              location.href
+                            ).toString(),
+                            title: bulletin.highlights,
+                            text: bulletin.highlights
+                          });
+                        }}
+                      >
+                        <svg
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-share"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3" />
+                        </svg>
+                      </a>
+                    </Tooltip>
+                  </li>
+                )}
+                <li>
                     <Tooltip
                       label={intl.formatMessage({
                         id: "bulletin:linkbar:pdf:hover"
