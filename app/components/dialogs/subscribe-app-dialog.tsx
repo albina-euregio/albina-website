f0a0a9ba0fb75ad488f76cc16045c239f53b36b6
Simon Legner <Simon.Legner@gmail.com>
2025-11-04 19:25:03 +0100

    chore: hide "Lawine Tirol App" for other provinces
    albina-euregio/albina-website#697

--- app/components/dialogs/subscribe-app-dialog.tsx
+++ app/components/dialogs/subscribe-app-dialog.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { useIntl } from "../../i18n";
 import { Tooltip } from "../tooltips/tooltip";
+import { useStore } from "@nanostores/react";
+import { $province } from "../../appStore.ts";
 
 export default function SubscribeAppDialog() {
   const intl = useIntl();
+  const province = useStore($province);
   const imgRoot = `${window.config.projectRoot}images/pro/apps/`;
 
-  const apps = config.subscribe.apps;
+  const apps = config.subscribe.apps.filter(
+    a => !province || !a.regions || a.regions.includes(province)
+  );
 
   return (
     <>
