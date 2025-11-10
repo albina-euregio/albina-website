ac9abad51f62ecf85c365ca28ec9cfa986083e25
Simon Legner <Simon.Legner@gmail.com>
2025-11-10 20:07:10 +0100

    chore: iframe script (base)

--- app/iframe.ts
+++ app/iframe.ts
@@ -5,14 +5,14 @@ initializeIFrame();
 
 function initializeIFrame() {
   const element = document.querySelector("[data-av-region]");
-  if (!element) return;
-  const region = element.dataset.avRegion;
-  const ratio = element.dataset.avRatio;
+  if (!(element instanceof HTMLElement)) return;
+  const province = element.dataset.avRegion || "";
+  const ratio = element.dataset.avRatio || "1/1";
   const date =
     element.dataset.avDate ||
     new URLSearchParams(location.search).get("date") ||
     "latest";
-  const lang = element.dataset.avLang || "de";
+  const language = element.dataset.avLang || "de";
   const id = element.dataset.avId || "albina-website-iframe";
 
   const iframe = document.createElement("iframe");
@@ -23,14 +23,15 @@ function initializeIFrame() {
   iframe.id = id;
   const params = new URLSearchParams({
     headless: "1",
-    province: region,
-    language: lang,
+    province,
+    language,
     "map-ratio": ratio
   });
+
+  let base = location.href;
   if (location.hostname === "localhost") {
-    iframe.src = `/bulletin/${date}?${params}`;
-    console.log(location.pathname);
-  } else if (/EUREGIO|AT-02|AT-07|IT-32-BZ|IT-32-TN/.exec(String(region))) {
+    base = new URL('/', location.href).toString();
+  } else if (/EUREGIO|AT-02|AT-07|IT-32-BZ|IT-32-TN/.exec(String(province))) {
     params.delete("language");
     const host = {
       en: "avalanche.report",
@@ -42,22 +43,23 @@ function initializeIFrame() {
       it: "valanghe.report"
     };
     if (location.pathname.startsWith("/beta")) {
-      iframe.src = `https://${host[lang]}/beta/bulletin/${date}?${params}`;
+      base = `https://${host[language]}/beta/`;
     } else if (location.pathname.startsWith("/dev")) {
-      iframe.src = `https://${host[lang]}/dev/bulletin/${date}?${params}`;
+      base = `https://${host[language]}/dev/`;
     } else {
-      iframe.src = `https://${host[lang]}/bulletin/${date}?${params}`;
+      base = `https://${host[language]}/`;
     }
   } else {
-    iframe.src = `https://lawinen-warnung.eu/bulletin/${date}?${params}`;
+    base = `https://lawinen-warnung.eu/`;
   }
+  iframe.src = new URL(`bulletin/${date}?${params}`, base).toString();
   element.appendChild(iframe);
 
   iframeResize(
     {
       license: "GPLv3",
       waitForLoad: true,
-      onScroll: function ({ top, left }) {
+      onScroll({ top, left }) {
         console.log("onScroll", top, left);
         window.scrollTo({
           top,
