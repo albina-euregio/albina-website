b1e055e1852c77b72010c2e9b8c25f551c015a3d
Simon Legner <Simon.Legner@gmail.com>
2025-12-05 16:46:42 +0100

    refact(stationDataStore): use zod to replace -777

--- app/stores/stationDataStore.ts
+++ app/stores/stationDataStore.ts
@@ -3,38 +3,42 @@ import { currentSeasonYear } from "../util/date-season";
 import { useCallback, useMemo, useState } from "react";
 import { z } from "zod/mini";
 
+const number = z
+  .nullish(z.number())
+  .check(z.overwrite(v => (v === -777 ? undefined : v)));
+const string = z.nullish(z.string());
 const FeaturePropertiesSchema = z.object({
-  $smet: z.nullish(z.string()),
-  $stationsArchiveFile: z.nullish(z.string()),
-  "LWD-Nummer": z.nullish(z.string()),
-  "LWD-Region": z.nullish(z.string()),
-  altitude: z.nullish(z.number()),
-  Beobachtungsbeginn: z.nullish(z.string()),
-  date: z.nullish(z.date()),
-  GS_O: z.nullish(z.number()),
-  GS_U: z.nullish(z.number()),
-  HS: z.nullish(z.number()),
-  HSD24: z.nullish(z.number()),
-  HSD48: z.nullish(z.number()),
-  HSD72: z.nullish(z.number()),
-  LD: z.nullish(z.number()),
-  LT_MAX: z.nullish(z.number()),
-  LT_MIN: z.nullish(z.number()),
-  LT: z.nullish(z.number()),
-  N24: z.nullish(z.number()),
-  N48: z.nullish(z.number()),
-  N6: z.nullish(z.number()),
-  N72: z.nullish(z.number()),
-  name: z.nullish(z.string()),
-  OFT: z.nullish(z.number()),
-  operator: z.nullish(z.string()),
-  operatorLink: z.nullish(z.string()),
-  plot: z.nullish(z.string()),
-  RH: z.nullish(z.number()),
-  TD: z.nullish(z.number()),
-  WG_BOE: z.nullish(z.number()),
-  WG: z.nullish(z.number()),
-  WR: z.nullish(z.number())
+  $smet: string,
+  $stationsArchiveFile: string,
+  "LWD-Nummer": string,
+  "LWD-Region": string,
+  altitude: number,
+  Beobachtungsbeginn: string,
+  date: string,
+  GS_O: number,
+  GS_U: number,
+  HS: number,
+  HSD24: number,
+  HSD48: number,
+  HSD72: number,
+  LD: number,
+  LT_MAX: number,
+  LT_MIN: number,
+  LT: number,
+  N24: number,
+  N48: number,
+  N6: number,
+  N72: number,
+  name: string,
+  OFT: number,
+  operator: string,
+  operatorLink: string,
+  plot: string,
+  RH: number,
+  TD: number,
+  WG_BOE: number,
+  WG: number,
+  WR: number
 });
 
 type FeatureProperties = z.infer<typeof FeaturePropertiesSchema>;
@@ -47,7 +51,7 @@ export class StationData {
   constructor(object: GeoJSON.Feature<GeoJSON.Point, FeatureProperties>) {
     this.id = object.id as string;
     this.geometry = object.geometry;
-    this.properties = object.properties;
+    this.properties = FeaturePropertiesSchema.parse(object.properties);
   }
   get lon() {
     return this.geometry.coordinates[0];
