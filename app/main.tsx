e60e381886126e7588a122530118c9ab747224d6
Simon Legner <Simon.Legner@gmail.com>
2025-12-05 09:22:06 +0100

    GitLab CI: build:ES-AR

--- app/main.tsx
+++ app/main.tsx
@@ -14,8 +14,8 @@ window["scroll_duration"] = 1000;
  * config.json is not bundled with the app to allow config editing without
  * redeploying the whole app.
  */
-const configRequest = import.meta.env.VITE_APP_REGION
-  ? import(`./config.${import.meta.env.VITE_APP_REGION}.json`)
+const configRequest = import.meta.env.APP_REGION
+  ? import(`./config.${import.meta.env.APP_REGION}.json`)
   : import.meta.env.BASE_URL === "/dev/"
     ? import("./config-dev.json")
     : import("./config.json");
