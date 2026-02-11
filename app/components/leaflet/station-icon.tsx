6ab48900202c87fd0245a0612dfa3a4753a1b61d
Norbert Lanzanasto <n.lanzanasto@gmail.com>
2026-02-11 11:55:40 +0100

    fix(weather-maps): null

--- app/components/leaflet/station-icon.tsx
+++ app/components/leaflet/station-icon.tsx
@@ -156,7 +156,7 @@ export default class StationIcon extends React.Component<Props> {
         {shouldShowWindArrow &&
           this.getWindArrow(this.props.direction + 180, fill)}
 
-        {this.getText(this.props.value.toString(), shouldShowCircle)}
+        {this.getText((this.props.value ?? "").toString(), shouldShowCircle)}
       </div>
     );
   }
