10aa79b440c7672a3f2682afa32a1aa5217e530e
Rainer Fabrizi <fabrizi@transporter.at>
2026-02-13 15:02:56 +0100

    Auto stash before merge of "feature/station-map-table" and "origin/feature/station-map-table"

--- app/views/stationDashboard.tsx
+++ app/views/stationDashboard.tsx
@@ -324,14 +324,18 @@ function StationDashboard(props) {
         className={`station-view-toggle${viewMode === "table" ? " is-table" : ""}`}
       >
         <button
-          className={viewMode === "map" ? "pure-button secondary" : "pure-button inverse"}
+          className={
+            viewMode === "map" ? "pure-button secondary" : "pure-button inverse"
+          }
           onClick={() => setViewMode("map")}
         >
           {intl.formatMessage({ id: "stations:view:map" })}
         </button>
         <button
           className={
-            viewMode === "table" ? "pure-button secondary" : "pure-button inverse"
+            viewMode === "table"
+              ? "pure-button secondary"
+              : "pure-button inverse"
           }
           onClick={() => setViewMode("table")}
         >
