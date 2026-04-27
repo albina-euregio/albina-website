b7149148af6f49e61fc19a2497b8a85ec3cf92e7
fynn_at_jackcachpot_de <fynn@jackcachpot.de>
2026-04-27 15:09:43 +0200

    fix(weatherMapStore): adjust the thresholds and colors for relative snow

--- app/stores/weatherMapStore.ts
+++ app/stores/weatherMapStore.ts
@@ -156,8 +156,9 @@ export const config = {
         updateTimesOffset: { "*": 24 },
         metaFiles: {},
         units: "%",
-        thresholds: [0, 30, 60, 90, 110, 140, 170, 200, 230],
+        thresholds: [0, 30, 60, 90, 110, 140, 170, 200],
         colors: {
+          0: [0, 255, 0],
           1: [255, 160, 160],
           2: [255, 210, 210],
           3: [255, 255, 179],
