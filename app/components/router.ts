79467a44aa063d189179114e3cc0430598943490
Simon Legner <Simon.Legner@gmail.com>
2026-08-18 17:57:15 +0200

    Revert "chore(router): import.meta.env.BASE_URL (click)"
    This reverts commit f93787fd432c80e41930fb940e9532d58ec54e20.

--- app/components/router.ts
+++ app/components/router.ts
@@ -1,77 +1,35 @@
-import { createRouter, redirectPage } from "@nanostores/router";
-import { onMount } from "nanostores";
+import { createRouter } from "@nanostores/router";
 
-const projectRoot = import.meta.env.BASE_URL as "/";
+const projectRoot = import.meta.env.BASE_URL as "/" | "/beta/" | "/dev/";
 
-export const $router = createRouter(
-  {
-    home: `${projectRoot}`,
-    bulletin: `${projectRoot}bulletin`,
-    bulletinDate: `${projectRoot}bulletin/:date`,
-    bulletinLatest: `${projectRoot}bulletin/latest`,
-    incidents: `${projectRoot}incidents`,
-    snowProfiles: `${projectRoot}profiles`,
-    weather: `${projectRoot}weather`,
-    weatherMap: `${projectRoot}weather/map/`,
-    weatherMapDomain: `${projectRoot}weather/map/:domain`,
-    weatherMapDomainTimestamp: `${projectRoot}weather/map/:domain/:timestamp/:timeSpan?`,
-    weatherArchive: `${projectRoot}weather/archive`,
-    weatherMeasurements: `${projectRoot}weather/measurements`,
-    weatherStations: `${projectRoot}weather/stations`,
-    conditions: `${projectRoot}conditions`,
-    conditionsProfiles: `${projectRoot}conditions/profiles`,
-    terrain: `${projectRoot}terrain`,
-    education: `${projectRoot}education`,
-    blogNamePost: `${projectRoot}blog/:blogName/:postId`,
-    blogTech: `${projectRoot}blog/tech`,
-    blog: `${projectRoot}blog`,
-    more: `${projectRoot}more`,
-    moreArchive: `${projectRoot}more/archive`,
-    moreLinkTree: `${projectRoot}more/linktree`,
-    archive: `${projectRoot}archive`,
-    educationStar: `${projectRoot}education/*`,
-    staticName: `${projectRoot}:name`,
-    staticSegmentName: `${projectRoot}:segment/:name`
-  },
-  { links: false } // override click handler to handle projectRoot
-);
-
-function click(event: MouseEvent) {
-  let link = (event.target as HTMLElement).closest("a");
-  if (
-    link &&
-    event.button === 0 && // Left mouse button
-    link.target !== "_blank" && // Not for new tab
-    link.origin === location.origin && // Not external link
-    link.rel !== "external" && // Not external link
-    link.target !== "_self" && // Now manually disabled
-    !link.download && // Not download link
-    !event.altKey && // Not download link by user
-    !event.metaKey && // Not open in new tab by user
-    !event.ctrlKey && // Not open in new tab by user
-    !event.shiftKey && // Not open in new window by user
-    !event.defaultPrevented // Click was not cancelled
-  ) {
-    event.preventDefault();
-    const hashChanged = location.hash !== link.hash;
-    const href =
-      link.href.startsWith(new URL(projectRoot, location.href).toString()) ||
-      !link.href.startsWith(new URL("/", location.href).toString())
-        ? link.href
-        : projectRoot + link.pathname.slice(1);
-    $router.open(href);
-    if (hashChanged) {
-      location.hash = link.hash;
-      if (link.hash === "" || link.hash === "#") {
-        window.dispatchEvent(new HashChangeEvent("hashchange"));
-      }
-    }
-  }
-}
-
-onMount($router, () => {
-  document.body.addEventListener("click", click);
-  return () => document.body.removeEventListener("click", click);
+export const $router = createRouter({
+  home: `${projectRoot}`,
+  bulletin: `${projectRoot}bulletin`,
+  bulletinDate: `${projectRoot}bulletin/:date`,
+  bulletinLatest: `${projectRoot}bulletin/latest`,
+  incidents: `${projectRoot}incidents`,
+  snowProfiles: `${projectRoot}profiles`,
+  weather: `${projectRoot}weather`,
+  weatherMap: `${projectRoot}weather/map/`,
+  weatherMapDomain: `${projectRoot}weather/map/:domain`,
+  weatherMapDomainTimestamp: `${projectRoot}weather/map/:domain/:timestamp/:timeSpan?`,
+  weatherArchive: `${projectRoot}weather/archive`,
+  weatherMeasurements: `${projectRoot}weather/measurements`,
+  weatherStations: `${projectRoot}weather/stations`,
+  conditions: `${projectRoot}conditions`,
+  conditionsProfiles: `${projectRoot}conditions/profiles`,
+  terrain: `${projectRoot}terrain`,
+  education: `${projectRoot}education`,
+  blogNamePost: `${projectRoot}blog/:blogName/:postId`,
+  blogTech: `${projectRoot}blog/tech`,
+  blog: `${projectRoot}blog`,
+  more: `${projectRoot}more`,
+  moreArchive: `${projectRoot}more/archive`,
+  moreLinkTree: `${projectRoot}more/linktree`,
+  archive: `${projectRoot}archive`,
+  educationStar: `${projectRoot}education/*`,
+  staticName: `${projectRoot}:name`,
+  staticSegmentName: `${projectRoot}:segment/:name`
 });
 
 export function redirectPageQuery(search: Record<string, string | number>) {
